adaa8d715dd907df719544e29b9a0fd067d8a804
yes6686 <127647511+yes6686@users.noreply.github.com>
2023-12-21 23:31:25 +0900

    [Silver V] Title: 영화감독 숌, Time: 56 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int dp[10001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	string s;
+	int k = 1;
+	for (int i = 600; i <= 3000000; i++) {
+		s = to_string(i);
+		for (int j = 0; j < s.length()-2; j++) {
+			if (s[j] == '6' && s[j + 1] == '6' && s[j + 2] == '6') {
+				dp[k] = i;
+				k++;
+				break;
+			}
+		}
+		if (k == n+1) {
+			cout <<dp[k - 1] << '\n';
+			break;
+		}
+	}
+	
+	
+}
