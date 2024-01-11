e458f391cbe056f3cc3abcbc28e45c0ebfd263d3
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-01-11 20:45:37 +0900

    [Gold IV] Title: 음식 평론가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1188. 음식 평론가/음식 평론가.cc
+++ 백준/Gold/1188. 음식 평론가/음식 평론가.cc
@@ -9,12 +9,5 @@ int main() {
 	int n, m;
 	cin >> n >> m;
 	int k = gcd(n, m);
-	if (n % m == 0) {
-		cout << 0 ;
-	}
-	else {
-		n /= k;
-		m /= k;
-		cout << k*(m - 1);
-	}
+	cout << k * (m/k - 1);
 }
