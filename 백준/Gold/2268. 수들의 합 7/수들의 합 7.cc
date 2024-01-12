80f8bf7c108c6e123823c25d1eb5c00302d669f2
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-01-12 20:47:53 +0900

    [Gold I] Title: 수들의 합 7, Time: 716 ms, Memory: 41084 KB -BaekjoonHub

---
+++ 백준/Gold/2268. 수들의 합 7/수들의 합 7.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+long long int arr[1000001];
+long long int t[4000001];
+
+long long int sum(int n, int s, int e,int l,int r) {
+	if (r < s || l > e) return 0;
+	if (l <= s && e <= r) return t[n];
+
+	return sum(n*2, s, (s + e) / 2, l, r) + sum(n*2+1, (s + e) / 2 + 1, e, l, r);
+}
+
+void modify(int n,int s,int e, int i, long long int diff) {
+	if (i < s || e < i) return;
+	t[n] += diff;
+	if (s != e) {
+		modify(2 * n, s, (s + e) / 2, i, diff);
+		modify(2 * n+1, (s+e)/2+1, e, i, diff);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n,m;
+	cin >> n>>m;
+	long long int a, b, c;
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b >> c;
+		if (a == 0) { // sum 함수를 리턴
+			if (b < c) {
+				cout << sum(1, 0, n - 1, b-1, c-1) << '\n';
+			}
+			else{
+				cout << sum(1, 0, n - 1, c-1, b-1) << '\n';
+			}
+		}
+		else if(a == 1){ // modify(b, c)를 수행
+			long long int d = c - arr[b-1];
+			arr[b-1] = c;
+			modify(1, 0, n - 1, b-1, d);
+		}
+	}
+}
