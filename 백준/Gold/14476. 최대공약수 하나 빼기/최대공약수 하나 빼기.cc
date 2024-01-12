2ec424cb2523683239245b4e81439c61f278753a
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-01-12 14:54:16 +0900

    [Gold II] Title: 최대공약수 하나 빼기, Time: 164 ms, Memory: 13740 KB -BaekjoonHub

--- 백준/Gold/14476. 최대공약수 하나 빼기/최대공약수 하나 빼기.cc
+++ 백준/Gold/14476. 최대공약수 하나 빼기/최대공약수 하나 빼기.cc
@@ -2,83 +2,55 @@
 #include <algorithm>
 using namespace std;
 
-int arr[10000001];
+int arr[1000001];
+int larr[1000001];
+int rarr[1000001];
 
 int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 	int n;
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 	}
-	sort(arr, arr + n);
-	int check = 0;
-	int index;
-	int x = gcd(arr[0], arr[1]);
-	if (x == 1) {
-		check = 1;
-		index = 0;
-		x = gcd(arr[1], arr[2]);
-		for (int i = 3; i < n; i++) {
-			x = gcd(x, arr[i]);
+	larr[0] = arr[0];
+	rarr[n - 1] = arr[n - 1];
+	for (int i = 1; i < n-1; i++) {
+		larr[i] = gcd(larr[i-1], arr[i]);
+	}
+	for (int i = n - 2; i >= 1; i--) {
+		rarr[i] = gcd(rarr[i+1], arr[i]);
+	}
+	int k=-1;
+	int maxGcd = -1;
+
+	if (arr[0] % rarr[1]) {
+		maxGcd = rarr[1];
+		k = arr[0];
+	}
+	if (arr[n - 1] % larr[n - 2]) {
+		if (maxGcd < larr[n - 2]) {
+			maxGcd = larr[n - 2];
+			k = arr[n - 1];
 		}
 	}
-	else {
-		int prev = x;
-		for (int i = 2; i < n; i++) {
-			x = gcd(x, arr[i]);
-			if (x == 1 && check==0) {
-				x = prev;
-				check = 1;
-				index = i;
+	
+	for (int i = 1; i < n - 2; i++) {
+		int x = gcd(larr[i - 1], rarr[i + 1]);
+		if (arr[i] % x) {
+			if (maxGcd < x) {
+				maxGcd = x;
+				k = arr[i];
 			}
-			prev = x;
 		}
 	}
-	if (check == 1 && x != 1) {
-		cout << x << ' ' << arr[index] << '\n';
-		return 0;
-	}
-	for (int i = 0; i < n; i++) {
-		arr[i] /= x;
-	}
-	int a = gcd(arr[0],arr[1]);
-	int b = gcd(arr[1],arr[2]);
-	int c = gcd(arr[2],arr[3]);
-	int aCnt = 0, bCnt = 0, cCnt = 0;
-	int aIndex = -1, bIndex = -1, cIndex = -1;
-	int ans = -1;
-	for (int i = 0; i < n; i++) {
-		if (arr[i] % a == 0) {
-			aCnt++;
-		}
-		else {
-			aIndex = i;
-		}
-		if (arr[i] % b == 0) {
-			bCnt++;
-		}
-		else {
-			bIndex = i;
-		}
-		if (arr[i] % c == 0) {
-			cCnt++;
-		}
-		else {
-			cIndex = i;
-		}
-	}
-	if (aCnt == n - 1) {
-		cout << x * a << ' ' << arr[aIndex] * x;
-	}
-	else if (bCnt == n - 1) {
-		cout << x * b << ' ' << arr[bIndex] * x;
-	}
-	else if (cCnt == n - 1) {
-		cout << x * c << ' ' << arr[cIndex] * x;
-	}
-	else {
+	if (k == -1) {
 		cout << -1;
 	}
+	else {
+		cout << maxGcd << ' ' << k;
+	}	
 }
