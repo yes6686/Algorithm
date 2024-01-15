ac5bcffdfa4b18bf5617e71a7d6cddd9a6997114
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-01-15 23:49:33 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <string>
+using namespace std;
+int arr[9];
+int n, m;
+queue<int>q;
+vector<int>v[10];
+int c[10];
+int d[10];
+void dfs(string s,int depth,int k) {
+	if (c[k]) return; 
+	c[k] = 1;
+	if (depth >= m) {
+		cout << s << '\n';
+
+		return;
+	}
+	int check = 1;
+	for (int i = 0; i < v[k].size(); i++) {
+		int y = v[k][i];
+		dfs(s + ' ' + to_string(y), depth + 1, y);
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i]-'0'==y) {
+				check = 0;
+			}
+		}
+		
+		if(check)
+		c[y] = 0;
+		else {
+			check = 1;
+		}
+	}
+
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> n >> m;
+	for (int i = 1; i < n; i++) {
+		for (int j = i+1; j <= n; j++) {
+			if (i != j) {
+				v[i].push_back(j);
+			}
+		}
+	}
+	for (int i = 1; i <= n; i++) {
+		dfs(to_string(i), 1, i);
+		for (int i = 0; i < 10; i++) {
+			c[i] = 0;
+		}
+	}
+		
+}
+
+	
