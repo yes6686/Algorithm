e832aa6fda3533c03bd19fdb7938d6111622fe1e
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-01-18 16:14:34 +0900

    [Silver I] Title: 1로 만들기 2, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -3,30 +3,33 @@
 using namespace std;
 
 int dp[1000001];
-string s[1000001];
 
 int main() {
 	int n;
 	cin >> n;
-	s[1] = "1";
+	for (int i = 2; i <= n; i++) dp[i] = i;
 	for (int i = 2; i <= n; i++) {
-		int  k;
-		dp[i] = dp[i - 1] + 1;
-		k = i - 1;
+		dp[i] = min(dp[i], dp[i - 1]+1);
 		if (i % 2 == 0) {
-			if (dp[i / 2]+1 < dp[i]) {
-				dp[i] = dp[i / 2]+1;
-				k = i / 2;
-			}
+			dp[i] = min(dp[i], dp[i / 2]+1);
 		}
 		if (i % 3 == 0) {
-			if (dp[i / 3]+1 < dp[i]) {
-				dp[i] = dp[i / 3]+1;
-				k = i / 3;
-			}
+			dp[i] = min(dp[i], dp[i / 3]+1);
 		}
-		s[i] += to_string(i)+" "+ s[k];
 	}
+
 	cout << dp[n] << '\n';
-	cout << s[n] << '\n';
+	while (1) {
+		cout << n << ' ';
+		if (n == 1) break;
+		if (dp[n] == dp[n - 1] + 1) {
+			n-=1;
+		}
+		else if (n % 2 == 0 && dp[n] == dp[n / 2] + 1) {
+			n /= 2;
+		}
+		else if (n % 3 == 0 && dp[n] == dp[n / 3] + 1) {
+			n /= 3;
+		}
+	}
 }
