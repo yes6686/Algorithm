8fcc5da821423fd07ad1c764ecac1267416e55fa
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-01-19 22:10:31 +0900

    [Silver III] Title: 피보나치 함수, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -1,50 +1,19 @@
 #include <iostream>
 using namespace std;
 
-long long int zeroCntDp[41];
-long long int oneCntDp[41];
-
-long long int zeroCntFibo(int n) {
-    if (zeroCntDp[n] != -1) return zeroCntDp[n];
-    if (n == 0) {
-        zeroCntDp[n] = 1;
-        return zeroCntDp[n];
-    }
-    else if (n == 1) {
-        zeroCntDp[n] = 0;
-        return zeroCntDp[n];
-    }
-    else {
-        return  zeroCntDp[n] = zeroCntFibo(n-1) + zeroCntFibo(n-2);
-    }
-}
-
-long long int oneCntFibo(int n) {
-    if (oneCntDp[n] != -1) return oneCntDp[n];
-    if (n == 0) {
-        oneCntDp[n] = 0;
-        return oneCntDp[n];
-    }
-    else if (n == 1) {
-        oneCntDp[n] = 1;
-        return oneCntDp[n];
-    }
-    else {
-        return  oneCntDp[n] = oneCntFibo(n - 1) + oneCntFibo(n - 2);
-    }
-}
+int dp[41] = { 0,1 };
 
 int main() {
-
-    for (int i = 0; i < 41; i++) {
-        zeroCntDp[i] = -1;
-        oneCntDp[i] = -1;
+    for (int i = 2; i < 41; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
     }
     int T;
     cin >> T;
     int x;
     while (T--) {
         cin >> x;
-        cout << zeroCntFibo(x) << ' ' << oneCntFibo(x) << '\n';
+        if (x == 0)  cout << 1 << ' ' << 0 << '\n';
+        else if (x == 1)  cout << 0 << ' ' << 1 << '\n';
+        else cout << dp[x - 1] << ' ' << dp[x] << '\n';
     }
 }
