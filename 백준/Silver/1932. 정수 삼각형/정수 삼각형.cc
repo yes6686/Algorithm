8ce3a8e4d9e8f1b89dc8d176838ffe2e079ae6b9
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-01-20 13:43:26 +0900

    [Silver I] Title: 정수 삼각형, Time: 36 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,13 +1,13 @@
 #include <iostream>
-
 using namespace std;
+
 int arr[501][501];
 int dp[501][501];
 int main() {
 	int n;
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < i+1; j++) {
+		for (int j = 0; j < i + 1; j++) {
 			cin >> arr[i][j];
 		}
 	}
@@ -15,27 +15,21 @@ int main() {
 	for (int i = 1; i < n; i++) {
 		for (int j = 0; j < i + 1; j++) {
 			if (j == 0) {
-				arr[i][j] += arr[i - 1][j];
-				dp[i][j] = arr[i][j];
+				dp[i][j] = arr[i][j] + dp[i - 1][j];
 			}
 			else if (j == i) {
-				arr[i][j] += arr[i - 1][j-1];
-				dp[i][j] = arr[i][j];
+				dp[i][j] = arr[i][j] + dp[i - 1][j - 1];
 			}
 			else {
-				arr[i][j] += max(arr[i - 1][j], arr[i - 1][j - 1]);
-				dp[i][j] = arr[i][j];
+				dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j])+arr[i][j];
 			}
 		}
 	}
-	int max = -1;
+	int maxAns = -1;
 	for (int i = 0; i < n; i++) {
-		if (max < arr[n - 1][i]) {
-			max = arr[n - 1][i];
+		if (maxAns < dp[n - 1][i]) {
+			maxAns = dp[n - 1][i];
 		}
 	}
-	cout << max;
-	
-
-
+	cout << maxAns;
 }
