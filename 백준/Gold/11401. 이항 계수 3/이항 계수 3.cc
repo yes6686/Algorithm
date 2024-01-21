118e4830e13d2c0054f379772c503ca767855637
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-01-21 23:58:17 +0900

    [Gold I] Title: 이항 계수 3, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	int n, r;
+	cin >> n >> r;
+	long long int a = 1;
+	long long int b = 1;
+	for (int i = 2; i <= n; i++) { // n!
+		a *= i;
+		a %= MOD;
+	}
+	for (int i = 2; i <= r; i++) { //r!
+		b *= i;
+		b %= MOD;
+	}
+	for (int i = 2; i <= n-r; i++) { //n!(n-r)!
+		b *= i;
+		b %= MOD;
+	}
+	long long int T = MOD - 2;
+	long long int ans = 1;
+	while (T) {
+		if (T % 2 == 1) {
+			ans = (ans * b) % MOD;
+		}
+		b = (b * b) % MOD;
+		T /= 2;
+	}
+	ans = (ans * a) % MOD;
+	cout << ans;
+}
