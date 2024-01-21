3e64a15339eff5c9abdbd390dd4abc4e0077d761
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-01-21 17:23:08 +0900

    [Silver II] Title: 연속합, Time: 32 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int arr[100001];
+int dp[100001];
+
+int main() {
+	int n;
+	cin >> n;
+	int isAllNegative = 1;
+	int maxAns = -1001;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		if (arr[i] > 0) isAllNegative = 0; // 원소 중 양수가 있나 체크
+		if (maxAns < arr[i]) {
+			maxAns = arr[i];
+		}
+	}
+	if (isAllNegative == 1) {
+		cout << maxAns << '\n';
+		return 0;
+	}
+	int sumGoing = 0;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] <0) {
+			maxAns = max(maxAns, sumGoing);
+		}
+		sumGoing += arr[i];
+		maxAns = max(maxAns, sumGoing);
+		if (sumGoing < 0) {
+			sumGoing = 0;
+		}
+	}
+	cout << maxAns;
+}
