bd5e4dbf579a3a5931e55a9abf56b51d4470110d
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-01-22 19:39:43 +0900

    [Silver III] Title: 파도반 수열, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -10,8 +10,7 @@ int main() {
 	dp[2] = 1;
 	dp[3] = 1;
 	dp[4] = 2;
-	dp[5] = 2;
-	for (int i = 6; i <= 100; i++) {
+	for (int i = 5; i <= 100; i++) {
 		dp[i] = dp[i - 1] + dp[i - 5];
 	}
 	while (T--) {
