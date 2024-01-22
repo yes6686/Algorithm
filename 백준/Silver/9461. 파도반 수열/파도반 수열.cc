4a4886b85f2fe562f7765e0c6c20ac292e537bf5
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-01-22 19:39:22 +0900

    [Silver III] Title: 파도반 수열, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -1,20 +1,22 @@
 #include <iostream>
 using namespace std;
-long long int arr[101];
-long long int recursion(int n) {
-	if (arr[n] != 0) return arr[n];
-	if (n <= 3) return arr[n]=1;
-	else if (n <= 5) return arr[n]=2;
-	else {
-		return arr[n] = recursion(n - 5) + recursion(n - 1);
-	}
-}
-int main() {
-	int T,N;
-	cin >> T;
 
+long long int dp[101];
+
+int main() {
+	int T;
+	cin >> T;
+	dp[1] = 1;
+	dp[2] = 1;
+	dp[3] = 1;
+	dp[4] = 2;
+	dp[5] = 2;
+	for (int i = 6; i <= 100; i++) {
+		dp[i] = dp[i - 1] + dp[i - 5];
+	}
 	while (T--) {
-		cin >> N;
-		cout << recursion(N) << endl;
+		int n;
+		cin >> n;
+		cout << dp[n] << '\n';
 	}
 }
