09a52a6932320a1edcac2ffa040405710d9fe83c
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-01-23 19:11:30 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[21];
+int visited[21];
+int n, s;
+
+int ans = 0;
+
+void solve(int depth, int a) {
+	if (a == s && depth) {
+		ans++;
+	}
+	if (depth >= n) return;
+	if (arr[depth] > 0 && a > s) return;
+	for (int i = depth+1; i <= n; i++) {
+		solve(i, a + arr[i-1]);
+	}
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> n >> s;
+	for (int i = 0; i< n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+	solve(0, 0);
+	cout << ans;
+
+}
