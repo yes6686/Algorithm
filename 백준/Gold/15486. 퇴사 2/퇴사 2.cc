2438341320d204e462ca1a481911723c4276475a
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-01-24 14:16:31 +0900

    [Gold V] Title: 퇴사 2, Time: 212 ms, Memory: 7880 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.cc
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -2,34 +2,23 @@
 using namespace std;
 
 int dp[1500001];
-int T[1500001];
-int p[1500001];
-
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 	int n;
 	cin >> n;
-	int k = 0;
-	int index = 0;
-	for (int i = 0; i < n; i++) {
-		cin >> T[i] >> p[i];
-
-	}
-	int result = 0;
-	int maxt = 0;
-	for (int i = 0; i <= n; i++) {
-		maxt = max(maxt, dp[i]);
-		if (i + T[i] >= n + 1) {
+	int t, p;
+    int maxAns = 0;
+	for (int i = 1; i <= n; i++) {
+		cin >> t >> p;
+		if (t + i > n + 1) {
+			dp[i] = max(dp[i], dp[i - 1]);
 			continue;
 		}
-		dp[i + T[i]] = max(dp[i + T[i]], maxt + p[i]);
-		result = max(result, dp[i + T[i]]);
-
+		dp[i] = max(dp[i], dp[i - 1]);
+		dp[t + i] = max(dp[t + i], dp[i]+p);
+		maxAns = max(maxAns, dp[t + i]);
 	}
-	
-	cout << result;
-
+	cout << maxAns;
 }
