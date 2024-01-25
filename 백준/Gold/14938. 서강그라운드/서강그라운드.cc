448c1ae9384664cf9b787b30e04a9f680a5c98d1
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-01-25 22:36:59 +0900

    [Gold IV] Title: 서강그라운드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.cc
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.cc
@@ -1,51 +1,59 @@
 #include <iostream>
-#define INF 1000000000
+#include <queue>
+#include <vector>
+#define INF 1e9
 using namespace std;
 
 int arr[101];
-int len[101][101];
+int dis[101];
+vector<pair<int, int>>v[101];
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
+
+int n, m, r;
+
+void djk(int x) {
+	for (int i = 1; i <= n; i++) {
+		dis[i] = INF;
+	}
+	pq.push({ 0,x });
+	dis[x] = 0;
+	while (!pq.empty()) {
+		int len = pq.top().first;
+		int y = pq.top().second;
+		pq.pop();
+		for (int i = 0; i < v[y].size(); i++) {
+			int k = v[y][i].second;
+			if (dis[k] > len + v[y][i].first) {
+				dis[k] = len + v[y][i].first;
+				pq.push({ dis[k],k });
+			}
+		}
+	}
+}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	int n, m, r;
 	cin >> n >> m >> r;
-
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (i != j) {
-				len[i][j] = INF;
-			}
-		}
-	}
-
 	for (int i = 1; i <= n; i++) {
 		cin >> arr[i];
 	}
-	int a, b, l; 
+	int a, b, l;
 	for (int i = 0; i < r; i++) {
 		cin >> a >> b >> l;
-		len[a][b] = l;
-		len[b][a] = l;
+		v[a].push_back({ l,b });
+		v[b].push_back({ l,a });
 	}
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			for (int t = 1; t <= n; t ++) {
-				if (len[j][i] + len[i][t] < len[j][t]) {
-					len[j][t] = len[j][i] + len[i][t];
-				}
-			}
-		}
-	}
-	int maxAns = 0;
+	int ans = 0;
 	for (int i = 1; i <= n; i++) {
 		int sum = 0;
+		djk(i);
 		for (int j = 1; j <= n; j++) {
-			if (len[i][j] <= m) {
+			if (dis[j] <= m) {
 				sum += arr[j];
 			}
 		}
-		maxAns = max(maxAns, sum);
+		ans = max(ans, sum);
 	}
-	cout << maxAns;
+	cout << ans;
 }
