cf35a87ab94c310fa6f5f1cc2ac40f9469391690
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-01-26 19:24:34 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2388 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.cc
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -2,9 +2,11 @@
 using namespace std;
 
 int arr[1001];
-int dp[1001][32][3];
+int dp[1001][31][3];
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 	int T, W;
 	cin >> T >> W;
 	for (int i = 0; i < T; i++) {
@@ -28,33 +30,40 @@ int main() {
 	}
 	for (int i = 1; i < T; i++) {
 		for (int j = 0; j <= W; j++) {
-			if (arr[i] == 1) {
-				if(dp[i-1][j][1]!=-1)
-					dp[i][j][1] = max(dp[i][j][1],dp[i-1][j][1] + 1);
-				
-				if (j >= 1) {
-					if (dp[i - 1][j - 1][2] != -1)
-						dp[i][j][1] = max(dp[i][j][1], dp[i - 1][j - 1][2] + 1);
-				}
-				
-				if (dp[i - 1][j][2] != -1)
-					dp[i][j][2] = max(dp[i][j][2], dp[i - 1][j][2]);
-			}
-			else { // arr[i] == 2
-				if (dp[i - 1][j][1] != -1)
-					dp[i][j][1] = max(dp[i][j][1], dp[i - 1][j][1]);
+			for (int t = 1; t <= 2; t++) {
+				if (arr[i] == 1) {
+					if (t == 1) {
+						if (dp[i - 1][j][t] != -1)
+							dp[i][j][t] = dp[i - 1][j][t] + 1;
 
-				if (dp[i - 1][j][2] != -1)
-					dp[i][j][2] = max(dp[i][j][2], dp[i - 1][j][2]+1);
-
-				if (j >= 1) {
-					if (dp[i - 1][j - 1][1] != -1)
-						dp[i][j][2] = max(dp[i][j][2], dp[i - 1][j - 1][1] + 1);
+						if (j != 0) {
+							if (dp[i - 1][j - 1][t + 1] != -1)
+								dp[i][j][t] = max(dp[i][j][t], dp[i - 1][j - 1][t + 1] + 1);
+						}					
+					}
+					else { // t==2
+						if(dp[i-1][j][t]!=-1)
+						dp[i][j][t] = dp[i - 1][j][t];
+					}
 				}
-			}
-			if (i == T - 1) {
-				maxAns = max(maxAns, dp[i][j][1]);
-				maxAns = max(maxAns, dp[i][j][2]);
+				else { // arr[i]==2
+					if (t == 1) {
+						if (dp[i - 1][j][t] != -1)
+						dp[i][j][t] = dp[i - 1][j][t];
+					}
+					else { // t==2
+						if (dp[i - 1][j][t] != -1)
+							dp[i][j][t] = dp[i - 1][j][t] + 1;
+                        
+						if (j != 0) {
+							if (dp[i - 1][j - 1][t - 1] != -1)
+								dp[i][j][t] = max(dp[i][j][t], dp[i - 1][j - 1][t - 1] + 1);
+						}
+					}
+				}
+				if (i == T - 1) {
+					maxAns = max(maxAns, dp[i][j][t]);
+				}
 			}
 		}
 	}
