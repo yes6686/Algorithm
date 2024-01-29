5b14dd0180c04b2950f0e66c07a1880bfe2fb7f7
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-01-29 20:16:18 +0900

    [Silver II] Title: 참외밭, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2477. 참외밭/참외밭.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+queue<pair<int, int>>q;
+queue<int>qq;
+queue<int>q1;
+queue<int>q2;
+
+int main() {
+	int k;
+	cin >> k;
+	int maxH = 0;
+	int maxW = 0;
+	int c;
+	int dis;
+
+	for (int i = 0; i < 6; i++) {
+		cin >> c >> dis;
+		q.push({ c,dis });
+		qq.push(dis);
+		if (c == 1 || c == 2) {
+			maxW = max(maxW, dis);
+		}
+		else {
+			maxH = max(maxH, dis);
+		}
+	}
+	while (!qq.empty()) {
+		int k = qq.front();
+		qq.pop();
+		q.push({1,k});
+	}
+	int preV = q.front().second;
+	q.pop();
+	int check = 0;
+	while (!q.empty()) {
+		int currV = q.front().second;
+		q.pop();
+		if ((preV == maxW && currV == maxH) ||
+			(preV == maxH && currV == maxW)) {
+			while (!q.empty()) {
+				currV = q.front().second;
+				q2.push(currV);
+				q.pop();
+			}
+			break;
+		}
+		q1.push(preV);
+		preV = currV;
+	}
+	int ans = 1;
+	int cnt = 0;
+	int multcheck = 0;
+	while (!q2.empty()) {
+		int k = q2.front();
+		q2.pop();
+		if (cnt == 1 || cnt == 2) {
+			ans *= k;
+		}
+		cnt++;
+	}
+	while (!q1.empty()) {
+		int k = q1.front();
+		q1.pop();
+		if (cnt == 1 || cnt == 2) {
+			ans *= k;
+		}
+		cnt++;
+	}
+	ans = maxH * maxW - ans;
+	ans *= k;
+	cout << ans;
+	//ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+}
