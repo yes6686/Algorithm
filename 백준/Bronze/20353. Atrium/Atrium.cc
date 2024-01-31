2471e166a6c94c803f1d5f77a813df886ed077d7
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-01-31 21:36:36 +0900

    [Bronze IV] Title: Atrium, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/20353. Atrium/Atrium.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double a;
+    cin >> a;
+    cout << fixed;
+    cout.precision(15);
+    cout << (sqrt(a) * 4);
+}
