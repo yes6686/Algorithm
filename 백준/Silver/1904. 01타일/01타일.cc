032f252fb5ff443b1d9837bb80848234647fc97a
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-01 18:54:24 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int dp[1000001];
+
+int main() {
+	int n;
+	cin >> n;
+	dp[1] = 1;
+	dp[2] = 2;
+	for (int i = 3; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+		dp[i] %= 15746;
+	}
+	cout << dp[n];
+}
