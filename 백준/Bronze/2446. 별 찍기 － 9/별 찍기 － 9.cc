d9e0e50a7ddb013dc049d0177acce14e90da6ca4
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-02 22:49:58 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i; j++) { //0 1 2 3 4
+			cout << " ";
+		}
+		for (int j = 0; j < n-i-1; j++) { //4 3 2 1 0
+			cout << "*";
+		}
+		cout << "*";
+		for (int j = 0; j < n-i-1; j++) { // 4 3 2 1 0 
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < n-1; i++) {
+		for (int j = 0; j < n-i-2; j++) { // 3 2 1 0
+			cout << " ";
+		}
+		for (int j = 0; j < i+1; j++) { //1 2 3 4
+			cout << "*";
+		}
+		cout << "*";
+		for (int j = 0; j < i+1; j++) { // 1 2 3 4
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
