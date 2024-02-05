016e0af7759ab276bf556d7b8f0ef35e76f60ed5
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-05 19:38:19 +0900

    [Gold II] Title: 트리의 지름, Time: 76 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<pair<int, int>>v[100001];
+int visited[100001];
+int edgeCnt[100001];
+
+int ans = 0;
+int ve = 0;
+
+void dfs(int x, int d, int p) {
+	if (ans < d) {
+		ans = d;
+		ve = x;
+	}
+	for (int i = 0; i < v[x].size(); i++) {
+		int cost = v[x][i].first;
+		int k = v[x][i].second;
+		if (k != p) {
+			dfs(k, d + cost, x);
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	int x;
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		while (1) {
+			int y,cost;
+			cin >> y;
+			if (y == -1) break;
+			else {
+				cin >> cost;
+			}
+			v[x].push_back({ cost,y });
+			edgeCnt[y]++;
+		}
+	}
+	dfs(1, 0, 1);
+	
+	dfs(ve, 0, ve);
+	
+	cout << ans;
+}
