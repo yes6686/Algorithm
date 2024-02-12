f8163816dd6177b3a9fe2e19f682669b15f45dc2
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-12 15:19:05 +0900

    [Bronze IV] Title: 모음의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	string s;
+	cin >> s;
+	int ans = 0;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+			ans++;
+		}
+	}
+	cout << ans;
+}
