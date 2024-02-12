df0b7900b40c5d67ad3efe472e40794e30928c99
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-12 15:13:24 +0900

    [Silver I] Title: 삼각 그래프, Time: 108 ms, Memory: 4364 KB -BaekjoonHub

--- 백준/Silver/4883. 삼각 그래프/삼각 그래프.cc
+++ 백준/Silver/4883. 삼각 그래프/삼각 그래프.cc
@@ -1,40 +1,46 @@
 #include <iostream>
 using namespace std;
 
-long long int arr[100001][3];
-long long int dp[100001][3];
+int arr[100001][3];
+int dp[100001][3];
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
 	int t = 1;
 	while (1) {
 		int n;
 		cin >> n;
-		if (!n) break;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < 3; j++) {
+		
+		if (n == 0) break;
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= 3; j++) {
 				cin >> arr[i][j];
 			}
 		}
-		dp[0][0] = arr[0][1];
-		dp[0][1] = arr[0][1];
-		dp[0][2] = arr[0][1] + arr[0][2];
-		for (int i = 1; i < n; i++) {
-			for (int j = 0; j < 3; j++) {
-				if (j == 0) {
-					dp[i][j] = min(dp[i-1][j], dp[i - 1][j + 1]) + arr[i][j];
+
+		dp[1][1] = arr[1][2];
+		dp[1][2] = arr[1][2];
+		dp[1][3] = arr[1][2] + arr[1][3];
+		
+		for (int i = 2; i <= n; i++) {
+			for (int j = 1; j <= 3; j++) {
+				if (j == 1) {
+					dp[i][j] = min(dp[i - 1][j], dp[i - 1][j + 1]) + arr[i][j];
 				}
-				else if (j == 1) {
-					dp[i][j] = min(dp[i-1][j-1],min(dp[i - 1][j], dp[i - 1][j + 1]))+arr[i][j];
-					dp[i][j] = min(dp[i][j], dp[i][j - 1] + arr[i][j]);
+				else if(j==2){
+					dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1]) + arr[i][j];
+					dp[i][j] = min(dp[i][j], dp[i][j - 1]+arr[i][j]);
+					dp[i][j] = min(dp[i][j], dp[i - 1][j + 1]+arr[i][j]);
 				}
 				else {
-					dp[i][j] = min(dp[i][j-1],min(dp[i - 1][j - 1], dp[i - 1][j]))+arr[i][j];
+					dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1]) + arr[i][j];
+					dp[i][j] = min(dp[i][j], dp[i][j - 1] + arr[i][j]);
 				}
 			}
 		}
-		cout <<t<<". " << dp[n - 1][1] << '\n';
-        t++;
+		cout << t<<". "<<dp[n][2] << '\n';
+
+
+		t++;
 	}
 }
