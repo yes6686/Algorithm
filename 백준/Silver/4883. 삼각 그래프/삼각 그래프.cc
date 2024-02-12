72466e8a81acba87f51c8a6f1b77cad4d905568d
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-12 15:14:02 +0900

    [Silver I] Title: 삼각 그래프, Time: 24 ms, Memory: 4364 KB -BaekjoonHub

--- 백준/Silver/4883. 삼각 그래프/삼각 그래프.cc
+++ 백준/Silver/4883. 삼각 그래프/삼각 그래프.cc
@@ -5,13 +5,13 @@ int arr[100001][3];
 int dp[100001][3];
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 	int t = 1;
 	while (1) {
 		int n;
 		cin >> n;
-		
 		if (n == 0) break;
-
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= 3; j++) {
 				cin >> arr[i][j];
@@ -39,8 +39,6 @@ int main() {
 			}
 		}
 		cout << t<<". "<<dp[n][2] << '\n';
-
-
 		t++;
 	}
 }
