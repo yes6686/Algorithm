09a8c83a7c439d252672cbccf117d3de2b1a180c
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-13 20:19:16 +0900

    [Bronze III] Title: Baseball, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10214. Baseball/Baseball.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T; cin >> T;
+	while (T--) {
+		int YwinCnt = 0;
+		int KwinCnt = 0;
+		for (int i = 0; i < 9; i++) {
+			int Y, K;
+			cin >> Y >> K;
+			YwinCnt+=Y;
+			KwinCnt+=K;
+		}
+		if (YwinCnt == KwinCnt) cout << "Draw" << '\n';
+		else if (YwinCnt > KwinCnt) cout << "Yonsei" << '\n';
+		else cout << "Korea" << '\n';
+	}
+}
