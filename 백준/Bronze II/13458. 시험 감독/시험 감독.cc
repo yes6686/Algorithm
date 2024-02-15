cbb703c047d663b29672a620f1c7e6f64c6dc02d
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-15 13:48:33 +0900

    [Bronze II] Title: 시험 감독, Time: 488 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Bronze II/13458. 시험 감독/시험 감독.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int arr[1000001];
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	int b, c;
+	cin >> b >> c;
+	long long int minAns = 0;
+	for (int i = 0; i < n; i++) {
+		arr[i] -= b;
+		minAns += 1;
+		if (arr[i] > 0) {
+			minAns += arr[i] / c; 
+			if (arr[i] % c != 0) {
+				minAns += 1;
+			}
+		}	
+	}
+	cout << minAns;
+}
