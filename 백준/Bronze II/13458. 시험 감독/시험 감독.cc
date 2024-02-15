e64c54428dce75e3c5476b29ed906acb44829bc6
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-15 14:00:09 +0900

    [Bronze II] Title: 시험 감독, Time: 492 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Bronze II/13458. 시험 감독/시험 감독.cc
+++ 백준/Bronze II/13458. 시험 감독/시험 감독.cc
@@ -4,25 +4,28 @@ using namespace std;
 int arr[1000001];
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	
 	int n;
 	cin >> n;
+	long long  sum = 0;
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
+
 	}
 	int b, c;
 	cin >> b >> c;
-	long long int minAns = 0;
 	for (int i = 0; i < n; i++) {
 		arr[i] -= b;
-		minAns += 1;
+		sum += 1;
 		if (arr[i] > 0) {
-			minAns += arr[i] / c; 
-			if (arr[i] % c != 0) {
-				minAns += 1;
+			if (arr[i] % c == 0) {
+				sum += arr[i] / c;
 			}
-		}	
+			else {
+				sum += arr[i] / c + 1;
+			}
+		}
 	}
-	cout << minAns;
+	cout << sum;
+
 }
