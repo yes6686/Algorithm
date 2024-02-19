57faf9cc553894f4bb6c44eea56d062d755dcb4c
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-19 20:15:59 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2420. 사파리월드/사파리월드.cc
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -1,21 +1,9 @@
 #include <iostream>
 using namespace std;
 
-
 int main() {
-
 	long long int n, m;
 	cin >> n >> m;
-	long long int result = n - m;
-
-	if (n - m < 0) {
-		cout << result*(- 1)<< endl;
-	}
-	else {
-		cout << result << endl;
-	}
-
-
-	
-	
+	long long int ans = abs(n - m);
+	cout << ans;
 }
