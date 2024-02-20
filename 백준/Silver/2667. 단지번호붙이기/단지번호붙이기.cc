77c3f486a891aec401049d70fffe0546ecdff0f5
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-20 19:23:50 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,23 +1,21 @@
 #include <iostream>
-#include <algorithm>
 #include <queue>
 using namespace std;
 
-string s[26];
 int arr[26][26];
-int dx[4] = { 0,0,1,-1 };
-int dy[4] = { 1,-1,0,0 };
-queue<pair<int, int>>q;
-int c[26][26];
-int n;
-int ch[626];
 
-int blockCnt = 1;
+queue<pair<int,int>>q;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0, 0,1,-1 };
+int visited[26][26];
 
-void bfs(int x, int y) {
+priority_queue<int,vector<int>, greater<int>>pq;
 
-	q.push(make_pair(x, y));
-	c[x][y] = 1;
+int bfs(int x,int y,int n) {
+	int cnt = 0;
+	q.push({ x,y });
+	cnt++;
+	visited[x][y] = 1;
 	while (!q.empty()) {
 		int kx = q.front().first;
 		int ky = q.front().second;
@@ -25,45 +23,40 @@ void bfs(int x, int y) {
 		for (int i = 0; i < 4; i++) {
 			int nx = kx + dx[i];
 			int ny = ky + dy[i];
-			if (nx <= 0 || ny <= 0 || nx > n || ny > n) continue;
-			if (c[nx][ny] || !arr[nx][ny]) continue;
-			q.push(make_pair(nx, ny)); blockCnt++;
-			c[nx][ny] = 1;
-
+			if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+			if (visited[nx][ny] || arr[nx][ny] == 0) continue;
+			visited[nx][ny] = 1;
+			q.push({ nx,ny });
+			cnt++;
 		}
-
 	}
-
-
+	return cnt;
 }
+
 int main() {
-	ios_base::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
 	cin.tie(NULL);
+	int n; 
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		cin >> s[i];
-		for (int j = 0; j < n; j++) {
-			arr[i + 1][j + 1] = s[i][j] - '0';
+		string s;
+		cin >> s;
+		for (int j = 0; j < s.size(); j++) {
+			arr[i][j] = s[j] - '0';
 		}
 	}
-	int cnt = 0;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j<=n; j++) {
-			if (arr[i][j] && !c[i][j]) {
-				blockCnt = 1;
-				cnt++;
-				bfs(i, j);
-				ch[cnt - 1] = blockCnt;
+	int count = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (arr[i][j] == 1 && visited[i][j] == 0) {
+				count++;
+				pq.push(bfs(i, j, n));
 			}
 		}
 	}
-	sort(ch, ch + cnt);
-
-	cout << cnt << '\n';
-	for (int i = 0; i < cnt; i++) {
-		cout << ch[i] << '\n';
+	cout << count << '\n';
+	while (!pq.empty()) {
+		cout << pq.top() << '\n';
+		pq.pop();
 	}
-
-
-
 }
