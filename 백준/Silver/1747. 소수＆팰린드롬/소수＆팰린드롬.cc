2c4c92db8fed24c30e1496bfaa26f40d1cef2e9e
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-21 15:04:00 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 192 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.cc
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.cc
@@ -12,7 +12,9 @@ bool isPrime(int x) {
 }
 
 bool isPalindrome(int x) {
+	
 	string s = to_string(x);
+
 	for (int i = 0; i < s.size() / 2; i++) {
 		if (s[i] != s[s.size() - i - 1]) {
 			return false;
@@ -28,16 +30,16 @@ int main() {
 	cin >> n;
 	if (n == 1) {
 		cout << 2;
+		return 0;
 	}
-	else {
-		while (1) {
-			if (isPrime(n) && isPalindrome(n)) {
-				cout << n << '\n';
-				break;
-			}
-			else {
-				n++;
-			}
+	while (1) {
+		if (isPrime(n) && isPalindrome(n)) {
+			cout << n << '\n';
+			break;
+		}
+		else {
+			n++;
 		}
 	}
+	
 }
