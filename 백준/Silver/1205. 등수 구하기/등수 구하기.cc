f0ecb9c5cdf55e0210d8bf8adf0520243f21b31f
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-22 13:51:15 +0900

    [Silver IV] Title: 등수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.cc
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.cc
@@ -1,54 +1,22 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
-
 int arr[51];
-int sarr[51];
-
-bool compare(int a, int b) {
-	return a > b;
-}
-
 int main() {
 	int n, s, p;
 	cin >> n >> s >> p;
-	if (n == 0) {
-		cout << 1<<'\n';
-		return 0;
-	}
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 	}
-	arr[n] = s;
-
-	sort(arr, arr + n + 1,compare);
-
-	sarr[0] = 1;
-
-	int k = 1;
-	int ans = 1;
-	int c = 0;
-
-	for (int i = 1; i <= n; i++) {
-
-		if (arr[i - 1] != arr[i]) {
-			sarr[i] = sarr[i - 1] + 1;
-			sarr[i] += c;
-			c = 0;
+	int cnt = 1;
+	int rank = 1;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] < s) break;	
+		if (arr[i] > s) {
+			rank++;
 		}
-		else {
-			sarr[i] = sarr[i - 1];
-			c++;
-		}
-		if (arr[i] == s) {
-			k = i + 1;
-			ans = sarr[i];
-		}
+		cnt++;
 	}
-	if (k <= p) {
-		cout << ans;
-	}
-	else {
+	if (cnt <= p) cout << rank;
+	else
 		cout << -1;
-	}
 }
