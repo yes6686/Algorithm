fe2db5bcb4da33ad8780b94804fb8d230983d9fc
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-24 00:48:32 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 20 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+queue<pair<pair<int, int>, int>>q;
+
+int n, k;
+int minAns = 1e9;
+
+int visited[500001][2];
+
+int bfs(int x, int k) {
+	q.push({ {x,k},0 });
+	visited[x][0] = 1;
+	while (!q.empty()) {
+		int kx = q.front().first.first;
+		int kk = q.front().first.second;
+		int s = q.front().second;
+		q.pop();
+		
+		if (kk > 500000) return -1;
+		if (visited[kk][s % 2]) {
+			return s;
+		}
+		if (kx - 1 > 0 && visited[kx - 1][(s + 1) % 2] == 0) {
+			visited[kx - 1][(s + 1) % 2] = 1;
+			q.push({ {kx - 1,kk + s+1},s + 1 });
+		}
+		if (visited[kx + 1][(s + 1) % 2] == 0) {
+			visited[kx + 1][(s + 1) % 2] = 1;
+			q.push({ {kx + 1,kk + s+1},s + 1 });
+		}
+		if (kx * 2 <= 500000 && visited[kx * 2][(s + 1) % 2] == 0) {
+			visited[kx * 2][(s+1) % 2] = 1;
+			q.push({ {kx * 2,kk + s+1},s + 1 });
+		}
+	}
+	return -1;
+}
+
+int main() {
+	cin >> n >> k;
+	cout<<bfs(n, k);
+}
