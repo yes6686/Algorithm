af44a3de55e7ed6f72f00f011366b94041dd0b36
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-24 15:37:11 +0900

    [Silver II] Title: 늑대와 올바른 단어, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/13022. 늑대와 올바른 단어/늑대와 올바른 단어.cc
+++ 백준/Silver/13022. 늑대와 올바른 단어/늑대와 올바른 단어.cc
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+
 	string s;
 	cin >> s;
 	int isFault = 0;
@@ -11,7 +9,6 @@ int main() {
 	int oCnt = 0;
 	int lCnt = 0;
 	int fCnt = 0;
-	int check = 0;
 	for (int i = 0; i < s.size(); i++) {
 
 		if (s[i] == 'w') {
@@ -19,14 +16,6 @@ int main() {
 				isFault = 1;
 				break;
 			}
-			if (check == 0) {
-				if (wCnt != oCnt || lCnt != fCnt || oCnt != lCnt) {
-					isFault = 1;
-					break;
-				}
-				check = 1;
-			}
-
 			wCnt++;
 		}
 		else if (s[i] == 'o') {
@@ -35,10 +24,6 @@ int main() {
 				break;
 			}
 			oCnt++;
-			if (wCnt < oCnt) {
-				isFault = 1;
-				break;
-			}
 
 		}
 		else if (s[i] == 'l') {
@@ -47,22 +32,18 @@ int main() {
 				break;
 			}
 			lCnt++;
-			if (oCnt < lCnt) {
-				isFault = 1;
-				break;
-			}
 		}
 		else if (s[i] == 'f') {
+			
 			if (wCnt != oCnt || oCnt != lCnt) {
 				isFault = 1;
 				break;
 			}
 			fCnt++;
-			if (lCnt < fCnt) {
+			if (fCnt > lCnt) {
 				isFault = 1;
 				break;
 			}
-			check = 0;
 		}
 	}
 	if (wCnt != oCnt || lCnt != fCnt || oCnt != lCnt) {
