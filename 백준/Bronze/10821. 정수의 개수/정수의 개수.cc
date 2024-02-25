e498f1873901e0d6dc1587a340b75029fd8d6318
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-25 23:06:44 +0900

    [Bronze II] Title: 정수의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10821. 정수의 개수/정수의 개수.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	string s;
+	cin >> s;
+	int cnt = 0;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == ',') cnt++;
+	}
+	cout << cnt + 1;
+}
