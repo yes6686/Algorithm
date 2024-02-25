faf72f60e443bceb3b53d1755e50893febb78dd6
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-25 21:00:39 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int arr[100001];
+
+int main() {
+	int n;
+	cin >> n;
+	int m;
+	cin >> m;
+	int prex = 0;
+	int minH = 0;
+	for (int i = 0; i < m; i++) {
+		int x;
+		cin >> x;
+		if (i == 0) {
+			if (x != 0) {
+				minH = max(minH, x - prex);
+			}
+		}
+		else {
+			minH = max(minH, (x - prex) / 2 + (x - prex) % 2);
+		}
+
+		prex = x;
+	}
+	if (prex != n) {
+		minH = max(minH, n - prex);
+	}
+	cout << minH;
+
+}
