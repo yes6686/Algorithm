529d043d87e3a6ab324ac6f8a2b767e436746d04
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-27 14:29:39 +0900

    [Gold III] Title: 내리막 길, Time: 24 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -4,7 +4,6 @@ using namespace std;
 
 int arr[501][501];
 int dp[501][501];
-
 int n, m;
 
 int dx[4] = { 1,-1,0,0 };
@@ -13,24 +12,16 @@ int dy[4] = { 0,0,1,-1 };
 int dfs(int x, int y) {
 	if (dp[x][y]!=-1) return dp[x][y];
 	if (x == n && y == m) return 1;
+	dp[x][y] = 0;
 
 	for (int i = 0; i < 4; i++) {
 		int nx = x + dx[i];
 		int ny = y + dy[i];
 		if (nx<1 || ny<1 || nx >n || ny>m) continue;
 		if (arr[x][y] <= arr[nx][ny]) continue;
-		if (dp[x][y] == -1) {
-			dp[x][y] = dfs(nx, ny);
-		}
-		else {
-			dp[x][y] += dfs(nx, ny);
-		}
-	}
-	if (dp[x][y] == -1) {
-		return 0;
+		dp[x][y] += dfs(nx, ny);
 	}
 	return dp[x][y];
-
 }
 
 int main() {
@@ -45,6 +36,4 @@ int main() {
 	}
 	memset(dp, -1, sizeof(dp));
 	cout<<dfs(1, 1);
-
-
 }
