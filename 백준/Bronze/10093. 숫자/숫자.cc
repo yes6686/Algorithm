6e0c5a708596d50dccd1cf239a16499bd6e489d3
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-27 19:57:41 +0900

    [Bronze II] Title: 숫자, Time: 16 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.cc
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -1,17 +1,13 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
-
-int arr[11];
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	int a, b;
+	long long int a, b;
 	cin >> a >> b;
 
 	if (a > b) {
-		int temp = a;
+		long long int temp = a;
 		a = b;
 		b = temp;
 	}
@@ -20,7 +16,7 @@ int main() {
 		return 0;
 	}
 	cout << b - a - 1 << '\n';
-	for (int i = a + 1; i < b; i++) {
+	for (long long int i = a + 1; i < b; i++) {
 		cout << i << ' ';
 	}
 }
