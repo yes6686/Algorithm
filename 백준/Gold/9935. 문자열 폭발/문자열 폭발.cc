8c23fe4d468a2e40e35d42b54caadf29619a284f
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-28 12:59:14 +0900

    [Gold IV] Title: 문자열 폭발, Time: 244 ms, Memory: 5048 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+using namespace std;
+
+stack<char>stk;
+stack<char>stk1;
+stack<char>ans;
+
+int main() {
+	string s;
+	cin >> s;
+	string b;
+	cin >> b;
+
+	string st = "";
+	for (int i = 0; i < s.size(); i++) {
+		st += s[i];
+		stk.push(s[i]);
+		if (st.size() != b.size())continue;
+		if (st == b ) {
+			for (int j = 0; j < b.size(); j++) {
+				stk.pop();
+			}
+			st = "";
+			for (int j = 0; j < b.size() - 1; j++) {
+				if (stk.size() == 0) break;
+				char k = stk.top();
+				stk.pop();
+				stk1.push(k);
+			}
+			while (!stk1.empty()) {
+				stk.push(stk1.top());
+				st += stk1.top();
+				stk1.pop();
+			}
+		
+		}
+		else {
+			string ccc = "";
+			for (int j = 1; j < st.size(); j++) {
+				ccc += st[j];
+			}
+			st = ccc;
+		}
+	}
+	if (stk.size() == 0) {
+		cout << "FRULA";
+		return 0;
+	}
+	while (!stk.empty()) {
+		ans.push(stk.top());
+		stk.pop();
+	}
+	while (!ans.empty()) {
+		cout << ans.top();
+		ans.pop();
+	}
+}
