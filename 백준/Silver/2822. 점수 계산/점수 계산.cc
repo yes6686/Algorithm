5137e8d1ebeeb46ee6bfc2d0e8b65a66061d06a3
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-02-29 18:13:19 +0900

    [Silver V] Title: 점수 계산, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/2822. 점수 계산/점수 계산.cc
@@ -0,0 +1,32 @@
+#include <iostream>	
+#include <algorithm>
+using namespace std;
+
+int arr[10000];
+int arr1[10];
+int result[6];
+int main() {
+
+	for (int i = 0; i < 8; i++) {
+		cin >> arr[i];
+		arr1[i] = arr[i];
+	}
+	sort(arr, arr + 8);
+	int sum = 0;
+	int k = 1;
+	for (int i = 3; i < 8; i++) {
+		sum += arr[i];
+		for (int j = 0; j < 8; j++) {
+			if (arr[i] == arr1[j]) {
+				result[k] = j+1;
+				k++;
+				
+			}
+		}
+	}
+	cout << sum << endl;
+	sort(result, result + 6);
+	for (int i = 1; i <= 5; i++) {
+		cout << result[i] << ' ';
+	}
+}
