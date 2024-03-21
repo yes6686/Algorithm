26b86d5cae458ea87a940645564d59ee28963617
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-03-21 19:46:33 +0900

    [Bronze IV] Title: FYI, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17863. FYI/FYI.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	string s;
+	cin >> s;
+	if (s[0] == '5' && s[1] == '5' && s[2] == '5') {
+		cout << "YES";
+	}
+	else {
+		cout << "NO";
+	}
+}
