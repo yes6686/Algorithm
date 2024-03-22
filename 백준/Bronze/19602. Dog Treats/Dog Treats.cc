16180ddfbcdb93cb03f535464139fc3ae761a70e
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-03-22 20:48:15 +0900

    [Bronze IV] Title: Dog Treats, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19602. Dog Treats/Dog Treats.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int s, m, l;
+	cin >> s >> m >> l;
+	if (s + 2 * m + 3 * l >= 10) {
+		cout << "happy";
+	}
+	else {
+		cout << "sad";
+	}
+}
