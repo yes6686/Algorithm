c4bd9485b533cec6a63595df55801e12764665de
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-04-02 15:02:07 +0900

    [Bronze III] Title: A+B - 6, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+	int n;
+	int a, b;
+	char c;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> c >> b;
+		cout << a + b << endl;
+	}
+}
