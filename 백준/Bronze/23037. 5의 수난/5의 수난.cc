89b128ad2649bf4a56b50029568f6b958750a82f
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-04-06 13:12:18 +0900

    [Bronze II] Title: 5의 수난, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/23037. 5의 수난/5의 수난.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	int sum = 0;
+	for (int i = 0; i < s.size(); i++) {
+		sum += pow(s[i] - '0', 5);
+	}
+	cout << sum;
+}
