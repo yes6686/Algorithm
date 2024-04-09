74fdbc08ef67dba5383289c352b67c620903856f
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-04-09 20:28:40 +0900

    [Bronze II] Title: 니모를 찾아서, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10173. 니모를 찾아서/니모를 찾아서.cc
+++ 백준/Bronze/10173. 니모를 찾아서/니모를 찾아서.cc
@@ -2,29 +2,23 @@
 #include <string>
 using namespace std;
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	while (1) {
-		string s;
-		getline(cin, s);
-		if (s == "EOI") break;
-		int check = 1;
-		for (int i = 0; i < s.size()-3; i++) {
-			string ss = "";
-			ss += tolower(s[i]);
-			ss += tolower(s[i+1]);
-			ss += tolower(s[i+2]);
-			ss += tolower(s[i+3]);
-			if ("nemo" == ss) {
-				check = 0;
-				cout << "Found" << '\n';
-				break;
-			}
-		}
-		if (check == 1) {
-			cout << "Missing" << '\n';
-		}
-	}
+    while (true)
+    {
+        string str;
+        getline(cin, str);
+
+        if (str == "EOI")
+            break;
+        for (int i = 0; i < str.size(); i++)
+            str[i] = toupper(str[i]);
+        if (str.find("NEMO") != string::npos)
+            cout << "Found" << "\n";
+        else
+            cout << "Missing" << "\n";
+    }
 }
