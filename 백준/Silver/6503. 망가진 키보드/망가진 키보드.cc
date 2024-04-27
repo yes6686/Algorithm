58638a9067a2b8aec2eb0277cbf0671757a8d2cf
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-04-27 22:54:19 +0900

    [Silver I] Title: 망가진 키보드, Time: 148 ms, Memory: 7128 KB -BaekjoonHub

---
+++ 백준/Silver/6503. 망가진 키보드/망가진 키보드.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+using namespace std;
+
+int arr[321123];
+
+int main() {
+    
+    
+    
+    
+	
+	while (1) {
+		int n;
+		cin >> n;
+		if (n == 0) break;
+		cin.ignore();
+		string s;
+		getline(cin, s);
+		int a = 0, b = 0;
+		int maxAns = 0;
+		int cnt = 0;
+		while (a<=b) {
+			if (b >= s.size()) break;
+			if (arr[s[b]]) {
+				arr[s[b]]++;
+				b++;
+				maxAns = max(maxAns, b - a);
+				continue;
+			}
+
+			if (cnt < n) {
+				arr[s[b]]++;
+				b++;
+				maxAns = max(maxAns, b - a);
+				cnt++;
+			}
+			else {
+				arr[s[a]]--;
+				if (arr[s[a]] == 0) {
+					cnt--;
+				}
+				a++;
+			}
+		}
+		memset(arr, 0, sizeof(arr));
+		cout << maxAns << '\n';
+	}
+}
