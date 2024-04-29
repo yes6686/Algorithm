79f4f8a6b1c80e43033325adfcb323fcc510f692
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-04-29 16:25:58 +0900

    [Silver III] Title: 1차원 2048, Time: 152 ms, Memory: 3044 KB -BaekjoonHub

--- 백준/Silver/27514. 1차원 2048/1차원 2048.cc
+++ 백준/Silver/27514. 1차원 2048/1차원 2048.cc
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+int arr[214223];
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	int n;
-	cin >> n;
+	int n; cin >> n;
 	long long int k = 0;
 	for (int i = 0; i < n; i++) {
 		long long int x;
@@ -15,20 +13,17 @@ int main() {
 			k = (k | x);
 		}
 		else {
-			while (1) {
-				if ((k & x) == 0)break;
+			while (k & x) {
 				k = (k ^ x);
 				x *= 2;
 			}
 			k = (k | x);
 		}
 	}
-
 	for (int i = 62;i>=0; i--) {
 		if (k & (long long)pow(2, i)) {
 			cout << (long long)pow(2, i);
 			return 0;
 		}
 	}
-
 }
