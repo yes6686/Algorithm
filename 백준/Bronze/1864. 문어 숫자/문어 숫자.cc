58ef305157220c13d6bbf5019d48a414a5d86ae7
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-05-02 23:39:35 +0900

    [Bronze II] Title: 문어 숫자, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Bronze/1864. 문어 숫자/문어 숫자.cc
+++ 백준/Bronze/1864. 문어 숫자/문어 숫자.cc
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <string>
 #include <cmath>
+#include <string>
 using namespace std;
-
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 	string s = "-\\(@?>&%/";
-	
 	while (1) {
 		string n;
 		cin >> n;
@@ -15,8 +15,6 @@ int main() {
 		for (int i = 0 ; i < n.size() ; i++) {
 			ans += (int)((s.find(n[i]) == 8) ? -1 : s.find(n[i]))* pow(8, n.size() - 1 - i);
 		}
-
 		cout << ans << '\n';
 	}
-
 }
