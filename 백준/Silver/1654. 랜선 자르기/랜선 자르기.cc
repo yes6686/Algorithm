89c8ac6388b8acfa6a377e9c7d4d03ee04f81804
yes6686 <127647511+yes6686@users.noreply.github.com>
2024-05-04 14:38:04 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,39 +1,37 @@
 #include <iostream>
-#include <algorithm>
-
 using namespace std;
-long long int arr[10001];
 
+int arr[10001];
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
 	int k, n;
 	cin >> k >> n;
-	int max = -1;
+	int maxL = 1;
 	for (int i = 0; i < k; i++) {
 		cin >> arr[i];
-		if (max < arr[i]) {
-			max = arr[i];
-		}
+		maxL = max(maxL, arr[i]);
 	}
-	long long int start = 1;
-	long long int end = max;
-	long long int cnt = 0;
-	long long int result=0;
-	long long int mid;
-	while (start <= end) {
-		mid = (start + end) / 2;
-		cnt = 0;
 
+	long long int s = 1;
+	long long int e = maxL;
+	long long int m = (s+e)/2;
+	int r = 0;
+	while (s<=e) {
+		m = (s + e) / 2;
+		long long int cnt = 0;
 		for (int i = 0; i < k; i++) {
-			cnt += arr[i] / mid;
+			cnt += (arr[i]) / m;
 		}
 		if (cnt < n) {
-			end = mid - 1;
+			e = m - 1;
 		}
 		else {
-			start = mid + 1;
-			result = mid;
+			s = m + 1;
+			r = m;
 		}
 	}
-	cout << result << endl;
+	cout << r;
 }
