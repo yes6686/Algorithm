6d9fa0f88a2be3636945e75126383fa36ddc5bcb
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-05-24 23:52:24 +0900

    [Silver II] Title: 예산, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.cc
+++ 백준/Silver/2512. 예산/예산.cc
@@ -1,51 +1,44 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int arr[10001];
+int arr[10000];
+int N, M;
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	int n;
-	cin >> n;
-	int sum = 0;
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-		sum += arr[i];
-		
-	}
-	int m;
-	cin >> m; 
-	
-	sort(arr, arr + n);
+int bSearch(int end)
+{
+        int start = 0;
+        int sum = 0;
+        int cost = 0;
+        while (start <= end) {
+                sum = 0;
+                cost = (start + end) / 2;
 
-	if (sum <= m) {
-		cout << arr[n - 1]<<'\n';
-		return 0;
-	}	
+                for (int i = 0; i < N; i++) {
+                        if (arr[i] < cost)
+                                sum += arr[i];
+                        else
+                                sum += cost;
+                }
 
-	int s = 0;
-	int e = arr[n-1];
-	int ans=0;
-	while (s <= e) {
-		sum = 0;
-		int mid = (s + e) / 2;
-		for (int i = 0; i < n; i++) {
-			if (arr[i] <= mid) {
-				sum += arr[i];
-			}
-			else {
-				sum += mid;
-			}
-		}
-		if (sum > m) {
-			e = mid - 1;
-		}
-		else {
-			s = mid + 1;
-			ans = max(ans, mid);
-		}
-	}
-	cout << ans;
+                if (sum <= M) {
+                        start = cost + 1;
+                }
+                else {
+                        end = cost - 1;
+                }
+        }
+
+        return end;
+}
+
+int main(void)
+{
+        scanf("%d", &N);
+        for (int i = 0; i < N; i++)
+                scanf("%d", &arr[i]);
+        scanf("%d", &M);
+
+        sort(arr, arr + N);
+        printf("%d", bSearch(arr[N - 1]));
+        return 0;
 }
