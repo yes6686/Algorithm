3ced85eef0d7367c8bbf6c8b86a2eff0acfa4913
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-05-28 12:28:40 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -1,12 +1,7 @@
 #include <iostream>
 using namespace std;
-
-int arr[30000000];
-
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	int a, b;
-	cin >> a >> b;
-	cout << a + b;
+	int A, B;
+	cin >> A >> B;
+	cout << A + B;
 }
