511b51fbb1656acd4e65c8adef585383ad9259bc
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-06-06 13:07:08 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n; 
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    int num; 
+    cin >> num;
+    cout << num << " " << num << "\n";
+  }
+}
