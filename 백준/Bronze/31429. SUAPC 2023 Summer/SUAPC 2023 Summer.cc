51038377b43c52ae22448b09cee9cee01a70a1da
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-06-19 12:33:05 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int s[]={12,11,11,10,9,9,9,8,7,6,6};
+    int p[]={1600,894,1327,1311,1004,1178,1357,837,1055,556,773};
+    int n;
+    cin>>n;
+    cout<<s[n-1]<<" "<<p[n-1];
+}
