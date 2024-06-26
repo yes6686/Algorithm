06e8e3d1fc7446df3ce10efa7285cd5b96e4be77
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-06-26 16:09:56 +0900

    [Bronze IV] Title: Dedupe, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/5357. Dedupe/Dedupe.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    prev = ''
+    s = input()
+    for i in range(len(s)):
+        if prev != s[i]:
+            print(s[i], end = "")
+            prev = s[i]
+    
+    print()
