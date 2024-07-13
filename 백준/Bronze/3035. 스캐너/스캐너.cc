1c673140136dcca25c148f7e1a1ba61876f34a7f
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-07-13 14:54:54 +0900

    [Bronze I] Title: 스캐너, Time: 4 ms, Memory: 2224 KB -BaekjoonHub

---
+++ 백준/Bronze/3035. 스캐너/스캐너.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+char arr[251][251];
+
+string str[251];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int r, c, zr, zc;
+    cin >> r >> c >> zr >> zc;
+
+    for (int i = 0; i < r; i++) {
+        string s;
+        cin >> s;
+        for (int j = 0; j < s.size(); j++) {
+            arr[i][j] = s[j];
+        }
+    }
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            for (int t = 0; t < zc; t++) {
+                str[i] += arr[i][j];
+            }
+        }
+    }
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < zr; j++) {
+            cout << str[i] << endl;
+        }
+    }
+}
