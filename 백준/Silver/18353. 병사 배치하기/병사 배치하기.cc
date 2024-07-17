e396a16b8db0228a062cd9add5d9e996bfd0ff2f
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-07-17 18:37:18 +0900

    [Silver II] Title: 병사 배치하기, Time: 4 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int arr[2001];
+int dp[2001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	int maxAns = 0;
+	for (int i = 0; i < n; i++) {
+		dp[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (arr[i] < arr[j]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		maxAns = max(maxAns, dp[i]);
+	}
+	cout << n - maxAns;
+}
