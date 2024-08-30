3164622494b3ff918b58863d473b611717aa1047
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-08-30 17:33:25 +0900

    [Silver III] Title: 이번에는 C번이 문자열, Time: 4 ms, Memory: 2480 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/29198. 이번에는 C번이 문자열/이번에는 C번이 문자열.cc
+++ 백준/Silver/29198. 이번에는 C번이 문자열/이번에는 C번이 문자열.cc
@@ -1,9 +1,9 @@
 #include <iostream>
+#include <string>
 #include <algorithm>
 using namespace std;
 
 string s[301];
-char str[90001];
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -13,16 +13,13 @@ int main() {
 	cin >> n >> m >> k;
 	for (int i = 0; i < n; i++) {
 		cin >> s[i];
+		sort(s[i].begin(), s[i].end());
 	}
 	sort(s, s + n);
 	string ss = "";
 	for (int i = 0; i < k; i++) {
 		ss += s[i];
 	}
-	for (int i = 0; i < ss.size(); i++) {
-		str[i] = ss[i];
-	}
-
-	sort(str, str + ss.size());
-	cout << str;
+	sort(ss.begin(), ss.end());
+	cout << ss;
 }
