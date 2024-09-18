c99e0d948fc17d75b157df96975f29fa1939f37f
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-09-18 14:51:40 +0900

    [Bronze IV] Title: Простая задача, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/22155. Простая задача/Простая задача.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n, i, f;
+    cin >> n;
+    while(n--){
+        cin >> i >> f;
+        if((i <= 1 && f <= 2) || (i <= 2 && f <= 1))
+            cout << "Yes\n";
+        else
+            cout << "No\n";
+    }
+}
