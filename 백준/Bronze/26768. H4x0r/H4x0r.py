4a4e361e4d25a2ce74fb18e2743ffef1cf1f0dd6
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-10-20 14:50:32 +0900

    [Bronze IV] Title: H4x0r, Time: 208 ms, Memory: 168536 KB -BaekjoonHub

---
+++ 백준/Bronze/26768. H4x0r/H4x0r.py
@@ -0,0 +1,10 @@
+def h4x0r(string):
+    replace_dict = {
+        "a": "4", "e": "3", "i": "1", "o": "0", "s": "5"
+    }
+ 
+    return "".join([replace_dict[word] if word in replace_dict else word for word in list(string)])
+if __name__ == "__main__":
+    string = input()
+    
+    print(h4x0r(string=string))
