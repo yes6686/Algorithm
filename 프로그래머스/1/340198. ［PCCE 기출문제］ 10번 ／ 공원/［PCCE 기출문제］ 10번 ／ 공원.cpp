3c4cbb17b73ac145d6e742b52a6004f5ab6e20a5
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-12-14 20:22:46 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 0.22 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.cpp
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.cpp
@@ -1,44 +1,54 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-int solution(vector<int> mats, vector<vector<string>> park) {
-    // 돗자리 크기 내림차순 정렬
-    sort(mats.begin(), mats.end(), greater<int>());
+int arr[51][51];
 
+bool compare(int a, int b){
+    return a>b;
+}
+
+int solution(vector<int> mats, vector<vector<string>> park) {
+    sort(mats.begin(), mats.end(), compare);
+    
     int rows = park.size();
     int cols = park[0].size();
-
-    // park 배열을 숫자로 변환 (0: 빈자리, 1: 사람이 있는 자리)
-    vector<vector<int>> arr(rows, vector<int>(cols, 0));
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            arr[i][j] = (park[i][j] == "-1" ? 0 : 1);
+    cout<<rows<<" , "<<cols<<'\n';
+    
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            if(park[i][j]=="-1"){
+                arr[i][j] = 0;
+            }else{
+                arr[i][j]=1;
+            }
+            cout<<arr[i][j]<<' ';
         }
+        cout<<'\n';
     }
-
-    // 가장 큰 돗자리부터 시도
-    for (int k : mats) {
-        for (int a = 0; a <= rows - k; a++) {
-            for (int b = 0; b <= cols - k; b++) {
-                bool canPlace = true;
-                for (int h = a; h < a + k && canPlace; h++) {
-                    for (int f = b; f < b + k; f++) {
-                        if (arr[h][f] == 1) {
-                            canPlace = false;
+    
+    
+    for(int k : mats){
+        for(int a=0;a<=rows-k;a++){
+            for(int b=0;b<=cols-k;b++){
+                int check=1;
+                for(int h=a;h<a+k && check==1;h++){
+                    for(int f=b;f<b+k;f++){
+                        if(arr[h][f]==1){
+                            check=0;
                             break;
                         }
                     }
                 }
-                if (canPlace) {
-                    return k; // 가장 큰 돗자리 크기 반환
+                if(check==1){
+                    return k;
                 }
             }
         }
     }
-
-    return -1; // 깔 수 있는 돗자리가 없음
+    
+    return -1;
 }
