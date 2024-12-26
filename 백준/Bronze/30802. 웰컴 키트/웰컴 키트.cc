91c00721977d9ebd9a22a75296eae1e50606338d
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-12-26 23:07:59 +0900

    [Bronze III] Title: 웰컴 키트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.cc
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.cc
@@ -1,25 +1,35 @@
+
 #include <iostream>
 using namespace std;
 
-int arr[6];
-
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	int n;
-	cin >> n;
-	for (int i = 0; i < 6; i++) {
-		cin >> arr[i];
-	}
-	int t, p;
-	cin >> t >> p;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	int minT = 0;
-	int maxP = n / p;
-	int pp = n % p;
+    int n;
+    cin >> n;
 
-	for (int i = 0; i < 6; i++) {
-		minT = (arr[i] % t==0) ? minT+arr[i]/t : minT+(arr[i]/t)+1;
-	}
-	cout << minT << '\n' << maxP << ' ' << pp << '\n';
+    int arr[6];
+    for (int i = 0; i < 6; i++) {
+        cin >> arr[i];
+    }
+
+    int t, p;
+    cin >> t >> p;
+
+    // 최소 작업 수 계산
+    int minTasks = 0;
+    for (int i = 0; i < 6; i++) {
+        minTasks += (arr[i] + t - 1) / t; // 올림하여 작업 수 계산
+    }
+
+    // n을 p로 나눈 몫과 나머지 계산
+    int maxGroups = n / p;
+    int remainingItems = n % p;
+
+    // 결과 출력
+    cout << minTasks << '\n';
+    cout << maxGroups << ' ' << remainingItems << '\n';
+
+    return 0;
 }
