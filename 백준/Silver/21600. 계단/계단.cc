f1ee67ae15f84ad7f2a962c568f14a6bcb1fd08c
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-12-29 18:36:01 +0900

    [Silver I] Title: 계단, Time: 12 ms, Memory: 2800 KB, Score: 60 point -BaekjoonHub

--- 백준/Silver/21600. 계단/계단.cc
+++ 백준/Silver/21600. 계단/계단.cc
@@ -1,4 +1,6 @@
+
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int arr[100001];
@@ -10,36 +12,24 @@ int main() {
 
     int n;
     cin >> n;
-    int maxAns = 1;
+
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    dp[0] = 1;
-    for (int i = 1; i < n; i++) {
-        if (arr[i - 1] < arr[i]) {
-            dp[i] = dp[i - 1] + 1;
-        }
-        else if(arr[i-1]==arr[i]) {
-            if (dp[i - 1] < arr[i - 1]) {
-                dp[i] = dp[i - 1] + 1;
-            }
-            else {
-                dp[i] = dp[i - 1];
-            }
-        }
-        else {
-            if (dp[i - 1] < arr[i]) {
-                dp[i] = dp[i - 1] + 1;
-            }
-            else if (dp[i - 1] == arr[i]) {
-                dp[i] = dp[i - 1];
-            }
-            else {
-                dp[i] = arr[i];
-            }
-        }
-        maxAns = max(maxAns, dp[i]);
-    }
-    cout << maxAns;
 
+    dp[0] = 1; // 첫 번째 계단의 길이는 1
+    int maxLength = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] >= dp[i - 1]) {
+            dp[i] = dp[i - 1] + 1;
+        } else {
+            dp[i] = arr[i];
+        }
+        maxLength = max(maxLength, dp[i]);
+    }
+
+    cout << maxLength << '\n';
+
+    return 0;
 }
