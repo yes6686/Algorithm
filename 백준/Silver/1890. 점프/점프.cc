050eb94044cc4db327de8b724bfcbb58661602ca
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-12-29 13:51:23 +0900

    [Silver I] Title: 점프, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+long long int arr[101][101];
+long long int dp[101][101];
+
+long long int dx[2] = { 1,0 };
+long long int dy[2] = { 0,1 };
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	dp[0][0] = 1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (i == n - 1 && j == n - 1) continue;
+			if (dp[i][j] == 0) continue;
+			
+			for (int k = 0; k < 2; k++) {
+				int nx = i + (dx[k] * arr[i][j]);
+				int ny = j + (dy[k] * arr[i][j]);
+				if (nx >= n || ny >= n) continue;
+				dp[nx][ny] += dp[i][j];
+			}
+		}
+	}
+
+	cout << dp[n - 1][n - 1] << '\n';
+}
