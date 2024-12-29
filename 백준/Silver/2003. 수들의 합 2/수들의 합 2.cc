a37036597f592530bef8ddefaf50f9eff0e19552
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-12-29 16:24:56 +0900

    [Silver IV] Title: 수들의 합 2, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -1,40 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int arr[10001];
-int s[10001];
+const int MAX_SIZE = 10001;
+int arr[MAX_SIZE];
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	int n, m;
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-		if (i == 0) s[i] = arr[i];
-		else {
-			s[i] = s[i - 1] + arr[i];
-		}
-	}
+    int arraySize, targetSum;
+    cin >> arraySize >> targetSum;
 
-	int ans = 0;
+    for (int i = 0; i < arraySize; i++) {
+        cin >> arr[i];
+    }
 
-	for (int i = n-1; i >= 0 ; i--) {
-		if (s[i] == m) {
-			ans++;
-			break;
-		}
-		else if (s[i] < m) break;
-		for (int j = 0; j < i; j++) {
-			if (s[i] - s[j] == m) {
-				ans++;
-				break;
-			}
-			else if (s[i] - s[j] < m) {
-				break;
-			}
-		}
-	}
-	cout << ans;
+    int start = 0, end = 0, currentSum = 0, count = 0;
+
+    while (end <= arraySize) {
+        if (currentSum < targetSum) {
+            currentSum += arr[end++];
+        }
+        else if (currentSum > targetSum) {
+            currentSum -= arr[start++];
+        }
+        else {
+            count++;
+            currentSum += arr[end++];
+        }
+    }
+
+    cout << count << '\n';
+
+    return 0;
 }
