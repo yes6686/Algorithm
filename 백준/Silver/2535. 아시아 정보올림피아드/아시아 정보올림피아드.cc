18283c3a1a1774f204445c2dba045c3d7d9d30e1
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-12-30 20:08:53 +0900

    [Silver V] Title: 아시아 정보올림피아드, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2535. 아시아 정보올림피아드/아시아 정보올림피아드.cc
+++ 백준/Silver/2535. 아시아 정보올림피아드/아시아 정보올림피아드.cc
@@ -2,30 +2,30 @@
 #include <algorithm>
 using namespace std;
 
-pair<int, pair<int, int>> arr[101];
+pair<int, pair<int, int>>arr[101];
 int cnt[101];
-int totalPriceCnt = 3;
-
 bool compare(pair<int, pair<int, int>> a, pair<int, pair<int, int>> b) {
-	return a.first > b.first;
+	return a.second.second > b.second.second;
 }
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-
+	
 	int n;
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		cin >> arr[i].second.first >> arr[i].second.second >> arr[i].first;
+		cin >> arr[i].first >> arr[i].second.first >> arr[i].second.second;
 	}
-	sort(arr, arr + n,compare);
+	sort(arr, arr + n, compare);
+	int totalCnt = 0;
 	for (int i = 0; i < n; i++) {
-		if (totalPriceCnt == 0) break;
-		if (cnt[arr[i].second.first] < 2) {
-			cnt[arr[i].second.first]++;
-			totalPriceCnt--;
-			cout << arr[i].second.first << ' ' << arr[i].second.second << '\n';
+		if (cnt[arr[i].first] >= 2) {
+			continue;
 		}
+		cnt[arr[i].first]++;
+		cout << arr[i].first << " " << arr[i].second.first << '\n';
+		totalCnt++;
+		if (totalCnt >= 3) break;
 	}
 }
