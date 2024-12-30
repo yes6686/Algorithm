bd70acf538ca7604fdbe939616fad081cc44da16
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-12-30 20:10:32 +0900

    [Silver V] Title: 아시아 정보올림피아드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2535. 아시아 정보올림피아드/아시아 정보올림피아드.cc
+++ 백준/Silver/2535. 아시아 정보올림피아드/아시아 정보올림피아드.cc
@@ -1,31 +1,48 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
-pair<int, pair<int, int>>arr[101];
-int cnt[101];
-bool compare(pair<int, pair<int, int>> a, pair<int, pair<int, int>> b) {
-	return a.second.second > b.second.second;
+struct Student {
+    int country;
+    int studentId;
+    int score;
+};
+
+bool compare(const Student &a, const Student &b) {
+    return a.score > b.score;
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i].first >> arr[i].second.first >> arr[i].second.second;
-	}
-	sort(arr, arr + n, compare);
-	int totalCnt = 0;
-	for (int i = 0; i < n; i++) {
-		if (cnt[arr[i].first] >= 2) {
-			continue;
-		}
-		cnt[arr[i].first]++;
-		cout << arr[i].first << " " << arr[i].second.first << '\n';
-		totalCnt++;
-		if (totalCnt >= 3) break;
-	}
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<Student> students(n);
+    vector<int> countryCount(101, 0);
+
+    for (int i = 0; i < n; i++) {
+        cin >> students[i].country >> students[i].studentId >> students[i].score;
+    }
+
+    sort(students.begin(), students.end(), compare);
+
+    int totalCnt = 0;
+    for (const auto &student : students) {
+        if (countryCount[student.country] >= 2) {
+            continue;
+        }
+
+        countryCount[student.country]++;
+        cout << student.country << " " << student.studentId << '\n';
+        totalCnt++;
+
+        if (totalCnt >= 3) {
+            break;
+        }
+    }
+
+    return 0;
 }
