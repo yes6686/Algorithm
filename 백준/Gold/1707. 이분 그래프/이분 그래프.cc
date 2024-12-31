ead4f34848b1a611fd7373d33091ce47b1d3c8e0
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-12-31 19:36:25 +0900

    [Gold IV] Title: 이분 그래프, Time: 212 ms, Memory: 11348 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.cc
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.cc
@@ -1,32 +1,19 @@
 #include <iostream>
-#include <string.h>
-#include <queue>
+#include <vector>
+#include <cstring>
 using namespace std;
 
-vector<int>v[20001];
+vector<int> v[20001];
 int visited[20001];
-int check = 0;
-void dfs(int a) {
-    for (int i = 0; i < v[a].size(); i++) {
-        int k = v[a][i];
-        if (visited[k] == 1) {
-            if (visited[a] == 1) {
-                check = 1;
-            }
-        }
-        else if (visited[k] == -1) {
-            if (visited[a] == -1) {
-                check = 1;
-            }
-        }
-        else {
-            if (visited[a] == 1) {
-                visited[k] = -1;
-            }
-            else {
-                visited[k] = 1;
-            }
-            dfs(k);
+bool isBipartite;
+
+void dfs(int node, int color) {
+    visited[node] = color;
+    for (int next : v[node]) {
+        if (visited[next] == 0) {
+            dfs(next, 3 - color);
+        } else if (visited[next] == color) {
+            isBipartite = false;
         }
     }
 }
@@ -40,29 +27,35 @@ int main() {
     while (T--) {
         int V, E;
         cin >> V >> E;
+
+        // 그래프 초기화
+        for (int i = 1; i <= V; i++) {
+            v[i].clear();
+            visited[i] = 0;
+        }
+
+        // 간선 입력
         for (int i = 0; i < E; i++) {
             int a, b;
             cin >> a >> b;
             v[a].push_back(b);
             v[b].push_back(a);
         }
-        check = 0;
+
+        isBipartite = true;
+
+        // 모든 정점에 대해 DFS 수행
         for (int i = 1; i <= V; i++) {
-            if (visited[i] == 0) {
-                visited[i] = 1;
-                dfs(i);
+            if (visited[i] == 0 && isBipartite) {
+                dfs(i, 1);
             }
         }
 
-        if (check == 1) {
-            cout << "NO" << '\n';
-        }
-        else {
-            cout << "YES" << '\n';
-        }
-        memset(visited, 0, sizeof(visited));
-        for (int i = 1; i <= V; i++) {
-            v[i].clear();
+        if (isBipartite) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
         }
     }
+    return 0;
 }
