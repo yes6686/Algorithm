af44b8a0acdfab8102d1ab5e52927158fab8d077
안예찬 <127647511+yes6686@users.noreply.github.com>
2024-12-31 18:17:07 +0900

    [Silver II] Title: 포켓볼, Time: 12 ms, Memory: 6124 KB -BaekjoonHub

--- 백준/Silver/4030. 포켓볼/포켓볼.cc
+++ 백준/Silver/4030. 포켓볼/포켓볼.cc
@@ -1,21 +1,20 @@
+
 #include <iostream>
 #include <map>
 #include <cmath>
 #define INF 1000000000
 using namespace std;
 
-
 int cnt4[100001];
-int check3[100001];
-map<long long int, int>mp4;
-map<long long int, int>mp3;
+map<int, int> mp3;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    long long int s = 1;
+    int s = 1;
 
+    // 삼각수를 미리 계산하여 map에 저장
     while (true) {
         long long int ss = (s * (s + 1)) / 2;
         if (ss > INF) {
@@ -24,6 +23,8 @@ int main() {
         mp3[ss] = 1;
         s++;
     }
+
+    // 제곱수 중 하나 적은 값이 삼각수인 것을 카운트
     s = 1;
     while (true) {
         long long int ss = s * s;
@@ -31,26 +32,22 @@ int main() {
             break;
         }
         if (mp3[ss - 1] == 1) {
-           // cout << "s : " << s << '\n';
             cnt4[s] = cnt4[s - 1] + 1;
-        }
-        else {
+        } else {
             cnt4[s] = cnt4[s - 1];
         }
         s++;
     }
 
-
+    // 각 입력 범위에 대한 결과 계산 및 출력
     int t = 1;
     while (true) {
         int a, b;
         cin >> a >> b;
         if (a == 0 && b == 0) break;
-        //cout << "a : "<<cnt4[(int)sqrt(a)] << '\n';
-        //cout << "b : "<<cnt4[(int)sqrt(b)] << '\n';
-        int cnt = cnt4[(int)sqrt(b-1)] - cnt4[(int)sqrt(a)];
+        
+        int cnt = cnt4[(int)sqrt(b - 1)] - cnt4[(int)sqrt(a)];
         cout << "Case " << t++ << ": " << cnt << '\n';
     }
-
-
+    return 0;
 }
