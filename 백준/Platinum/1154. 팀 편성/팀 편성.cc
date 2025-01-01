cf8df20d740e2f5708320ae7fa4274fa5e3f60b9
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-01-01 17:09:31 +0900

    [Platinum V] Title: 팀 편성, Time: 76 ms, Memory: 6544 KB -BaekjoonHub

--- 백준/Platinum/1154. 팀 편성/팀 편성.cc
+++ 백준/Platinum/1154. 팀 편성/팀 편성.cc
@@ -24,156 +24,92 @@ int main() {
         v[b].push_back(a);
     }
     checkA[1] = 1;
+    int count = 2;
+    while (count--) {
+        for (int i = 1; i <= n; i++) {
 
-    for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < v[i].size(); j++) {
+                int k = v[i][j];
 
-        for (int j = 0; j < v[i].size(); j++) {
-            int k = v[i][j];
-
-            ch[k] = 1;
-        }
-        ch[i] = 1;
-        if (checkA[i] == 1) {
-            for (int j = 1; j <= n; j++) {
-                if (ch[j] == 0) {
-
-                    if (checkA[j] == 1) {
-                        cout << -1 << '\n';
-                        return 0;
-                    }
-                    checkB[j] = 1;
-                }
+                ch[k] = 1;
             }
+            ch[i] = 1;
+            if (checkA[i] == 1) {
+                for (int j = 1; j <= n; j++) {
+                    if (ch[j] == 0) {
 
-        } else if (checkB[i] == 1) {
-            for (int j = 1; j <= n; j++) {
-                if (ch[j] == 0) {
-
-                    if (checkB[j] == 1) {
-                        cout << -1 << '\n';
-                        return 0;
-                    }
-                    checkA[j] = 1;
-                }
-            }
-        } else {
-
-            int chA = 0;
-            int chB = 0;
-
-            for (int j = 1; j <= n; j++) {
-                if (ch[j] == 0) {
-
-                    if (checkB[j] == 1) {
-                        if (chA == 1) {
+                        if (checkA[j] == 1) {
                             cout << -1 << '\n';
                             return 0;
                         }
-                        chB = 1;
-                    } else if (checkA[j] == 1) {
-                        if (chB == 1) {
+                        checkB[j] = 1;
+                    }
+                }
+
+            }
+            else if (checkB[i] == 1) {
+                for (int j = 1; j <= n; j++) {
+                    if (ch[j] == 0) {
+
+                        if (checkB[j] == 1) {
                             cout << -1 << '\n';
                             return 0;
                         }
-                        chA = 1;
-
+                        checkA[j] = 1;
                     }
                 }
             }
-            if (chB == 1) {
-                checkA[i] = 1;
-            } else if (chA == 1) {
-                checkB[i] = 1;
-            }
-            for (int j = 1; j <= n; j++) {
-                if (ch[j] == 1) continue;
+            else {
+
+                int chA = 0;
+                int chB = 0;
+
+                for (int j = 1; j <= n; j++) {
+                    if (ch[j] == 0) {
+
+                        if (checkB[j] == 1) {
+                            if (chA == 1) {
+                                cout << -1 << '\n';
+                                return 0;
+                            }
+                            chB = 1;
+                        }
+                        else if (checkA[j] == 1) {
+                            if (chB == 1) {
+                                cout << -1 << '\n';
+                                return 0;
+                            }
+                            chA = 1;
+
+                        }
+                    }
+                }
                 if (chB == 1) {
-                    checkB[j] = 1;
-                } else if (chA == 1) {
-                    checkA[j] = 1;
+                    checkA[i] = 1;
                 }
-            }
-
-        }
-        memset(ch, 0, sizeof(ch));
-
-    }
-    for (int i = 1; i <= n; i++) {
-
-        for (int j = 0; j < v[i].size(); j++) {
-            int k = v[i][j];
-
-            ch[k] = 1;
-        }
-        ch[i] = 1;
-        if (checkA[i] == 1) {
-            for (int j = 1; j <= n; j++) {
-                if (ch[j] == 0) {
-
-                    if (checkA[j] == 1) {
-                        cout << -1 << '\n';
-                        return 0;
-                    }
-                    checkB[j] = 1;
+                else if (chA == 1) {
+                    checkB[i] = 1;
                 }
-            }
 
-        } else if (checkB[i] == 1) {
-            for (int j = 1; j <= n; j++) {
-                if (ch[j] == 0) {
-
-                    if (checkB[j] == 1) {
-                        cout << -1 << '\n';
-                        return 0;
-                    }
-                    checkA[j] = 1;
-                }
-            }
-        } else {
-
-            int chA = 0;
-            int chB = 0;
-
-            for (int j = 1; j <= n; j++) {
-                if (ch[j] == 0) {
-
-                    if (checkB[j] == 1) {
-                        if (chA == 1) {
-                            cout << -1 << '\n';
-                            return 0;
-                        }
-                        chB = 1;
-                    } else if (checkA[j] == 1) {
-                        if (chB == 1) {
-                            cout << -1 << '\n';
-                            return 0;
-                        }
-                        chA = 1;
-
+                if (count == 0) {
+                    if (chB == 0 && chA == 0) {
+                        checkA[i] = 1;
                     }
                 }
-            }
-            if (chB == 1) {
-                checkA[i] = 1;
-            } else if (chA == 1) {
-                checkB[i] = 1;
-            } else {
-
-                checkA[i] = 1;
-            }
-            for (int j = 1; j <= n; j++) {
-                if (ch[j] == 1) continue;
-                if (chB == 1) {
-                    checkB[j] = 1;
-                } else if (chA == 1) {
-                    checkA[j] = 1;
+                for (int j = 1; j <= n; j++) {
+                    if (ch[j] == 1) continue;
+                    if (chB == 1) {
+                        checkB[j] = 1;
+                    }
+                    else if (chA == 1) {
+                        checkA[j] = 1;
+                    }
                 }
-            }
 
+            }
+            memset(ch, 0, sizeof(ch));
 
         }
-        memset(ch, 0, sizeof(ch));
-
     }
     cout << 1 << '\n';
     for (int i = 1; i <= n; i++) {
@@ -189,5 +125,4 @@ int main() {
         }
     }
     cout << -1 << '\n';
-
 }
