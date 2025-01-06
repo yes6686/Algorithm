9fcacb3145f78d83a2abf2947bf34b78eac3148a
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-01-06 20:03:02 +0900

    [Bronze I] Title: APC는 왜 서브태스크 대회가 되었을까?, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/17224. APC는 왜 서브태스크 대회가 되었을까？/APC는 왜 서브태스크 대회가 되었을까？.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+pair<int, int>arr[101];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n,l,k;
+	cin >> n>>l>>k;
+	int sum = 0;
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i].second >> arr[i].first;
+	}
+	sort(arr, arr + n);
+	for (int i = 0; i < n; i++) {
+		if (arr[i].first <= l) {
+			sum += 140;
+			cnt++;
+		}
+		else if (arr[i].second <= l) {
+			sum += 100;
+			cnt++;
+		}
+		if (cnt >= k) break;
+	}
+	cout << sum << '\n';
+}
