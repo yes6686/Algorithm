357d7a55669407d6c59a41298b872744b7daa9a3
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-01-15 21:59:05 +0900

    [Bronze V] Title: 새싹, Time: 160 ms, Memory: 17456 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
