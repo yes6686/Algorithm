f848a1125959d63174982d5fbd6ed3f2fb75c91c
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-01-23 19:45:06 +0900

    [Gold III] Title: 아침 산책, Time: 152 ms, Memory: 23504 KB, Score: 3 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.cc
+++ 백준/Gold/21606. 아침 산책/아침 산책.cc
@@ -5,21 +5,44 @@ using namespace std;
 
 int arr[200001];
 
+
+int combination(int n, int r) {
+	if (n == r || r == 0) {
+		return 1;
+	}
+	else {
+		return combination(n - 1, r - 1) + combination(n - 1, r);
+	}
+}
+
 vector<int>v[200001];
+vector<int>rv[200001];
 int visited[200001];
 
 int ans;
+int cnt = 0;
 
-void dfs(int n) {
+void go(int n) {
 	for (int i = 0; i < v[n].size(); i++) {
 		int k = v[n][i];
+		if (visited[k] == 0) {
+			visited[k] = 1;
+			rv[n].push_back(k);
+			go(k);
+		}
+	}
+}
+
+void dfs(int n) {
+	for (int i = 0; i < rv[n].size(); i++) {
+		int k = rv[n][i];
 		if (visited[k] == 0) {
 			visited[k] = 1;
 			if (arr[k] == 0) {
 				dfs(k);
 			}
 			else {
-				ans++;
+				cnt++;
 			}
 
 		}
@@ -45,13 +68,19 @@ int main() {
 		v[a].push_back(b);
 		v[b].push_back(a);
 	}
+	visited[1] = 1;
+	go(1);
 
 	for (int i = 1; i <= n; i++) {
 		if (arr[i] == 1) {
+			cnt = 0;
+			memset(visited, 0, sizeof(visited));
 			visited[i] = 1;
 			dfs(i);
-			memset(visited, 0, sizeof(visited));
+			if (cnt == 0) continue;
+			ans += combination(cnt + 1, 2)*2;
 		}
 	}
 	cout << ans<<'\n';
+
 }
