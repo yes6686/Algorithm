c557f9a994934df747f826ac998c1971ad4542c6
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-01-26 16:07:19 +0900

    [Silver I] Title: 컴백홈, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.cc
+++ 백준/Silver/1189. 컴백홈/컴백홈.cc
@@ -1,29 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int dx[4] = { 0,0,-1,1 };
+int arr[6][6];
+int visited[6][6];
+
+int dx[4] = { 0,0,1,-1 };
 int dy[4] = { 1,-1,0,0 };
 
-int arr[5][5];
-int visited[5][5];
 
 int r, c, k;
 int ans = 0;
 
-void bfs(int x, int y, int d) {
-	if (d > k) return;
-	if (d == k && x == 0 && y == c - 1) {
-		ans++;
-		return;
+void go(int a,int b,int d) {
+
+	if (a == 0 && b == c - 1) {
+
+		if (k == d) ans++;
+
+		return;	
 	}
+
 	for (int i = 0; i < 4; i++) {
-		int nx = x + dx[i];
-		int ny = y + dy[i];
+		int nx = dx[i] + a;
+		int ny = dy[i] + b;
 		if (nx < 0 || ny < 0 || nx >= r || ny >= c) continue;
-		if (arr[nx][ny] == -1) continue;
-		if (visited[nx][ny] == 1) continue;
+		if (visited[nx][ny] == 1 || arr[nx][ny] == 1) continue;
 		visited[nx][ny] = 1;
-		bfs(nx, ny, d + 1);
+		go(nx, ny, d + 1);
 		visited[nx][ny] = 0;
 	}
 }
@@ -31,20 +34,20 @@ void bfs(int x, int y, int d) {
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
+
 	cin >> r >> c >> k;
+
 	for (int i = 0; i < r; i++) {
 		string s;
 		cin >> s;
 		for (int j = 0; j < s.size(); j++) {
-			if (s[j] == '.') {
-				arr[i][j] = 0;
-			}
-			else {
-				arr[i][j] = -1;
+			if (s[j] == 'T') {
+				arr[i][j] = 1;
 			}
 		}
 	}
 	visited[r - 1][0] = 1;
-	bfs(r-1, 0, 1);
-	cout << ans;
+	go(r - 1, 0, 1);
+
+	cout << ans << '\n';
 }
