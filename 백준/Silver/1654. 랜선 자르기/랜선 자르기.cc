87049c3adc650118988facd49c89a5ac8231ac17
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-01-30 20:47:40 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,141 +1,37 @@
 #include <iostream>
-#include <cstdlib>
+#include <algorithm>
 using namespace std;
 
-int lanCable[10000];
+long long int arr[10001];
 
-// 분화되는 막대 수를 구한다.
-int getCount(int K, int searchPoint) {
-    int count = 0;
-    for (int i = 0; i < K; i++)
-    {
-        count += lanCable[i] / searchPoint;
-    }
-    return count;
-}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
-// 중간 값을 구한다.
-int getSearchPoint(int searchStart, int searchEnd) {
-    return searchStart + (searchEnd - searchStart) / 2;
-}
+	int K, N;
+	cin >> K >> N;
 
-// count 값이 N보다 크다면 1(searchPoint가 N보다 작다는 뜻) 작으면 -1(searchPoint가 N보다 크다는 뜻) 같다면 0을 리턴
-int getResult(int count, int N) {
-    if (N < count) {
-        return 1;
-    }
-    else if (N > count) {
-        return -1;
-    }
-    if (N == count) {
-        return 0;
-    }
-}
-
-int algorithm(int searchPoint, int searchStart, int searchEnd, int K, int N, int maxLength) {
-    int count;
-
-    while (true)
-    {
-        // 중간 값을 가져온다.
-        searchPoint = getSearchPoint(searchStart, searchEnd);
-
-        // 중간 값의 막대 수를 구한다.
-        count = getCount(K, searchPoint);
-
-        // 카운트 값을 비교하여 계산한다.
-        switch (getResult(count, N)) {
-        case 1:
-            // 상단부 탐색을 위해 최소 범위를 중간값으로 이동
-            if (searchStart < searchEnd) {
-                // 상단부로 이동해서 계산해야 되는데 조건식이 말단인 경우(다음 루프에서 반복적으로 한칸 내려옴)
-                if (searchEnd - searchPoint == 1) {
-                    // N개 이상으로 나눌 수 있을 때 최대 값인 경우
-                    if (getCount(K, searchEnd) != N && getCount(K, searchEnd) != N) {
-                        cout << searchPoint << "\n";
-                        exit(0);
-                    }
-                    cout << searchEnd << "\n";
-                    exit(0);
-                }
-                searchStart = searchPoint;
-            }
-            else {
-                // 상단부로 이동해서 계산해야 되는데 조건식이 말단인 경우(다음 루프에서 반복적으로 한칸 내려옴)
-                if (searchStart - searchPoint == 1) {
-                    // N개 이상으로 나눌 수 있을 때 최대 값인 경우
-                    if (getCount(K, searchStart) != N && getCount(K, searchEnd) != N) {
-                        cout << searchPoint << "\n";
-                        exit(0);
-                    }
-                    cout << searchStart << "\n";
-                    exit(0);
-                }
-                searchEnd = searchPoint;
-            }
-            break;
-        case -1:
-            // 하단부 탐색을 위해 최대 범위를 중간값으로 이동
-            if (searchStart < searchEnd) {
-                // 하단로 이동해서 계산해야 되는데 조건식이 말단인 경우(다음 루프에서 반복적으로 한칸 올라감)
-                if (searchPoint - searchStart == 1) {
-                    cout << searchStart << "\n";
-                    exit(0);
-                }
-                searchEnd = searchPoint;
-            }
-            else {
-                // 하단로 이동해서 계산해야 되는데 조건식이 말단인 경우(다음 루프에서 반복적으로 한칸 올라감)
-                if (searchPoint - searchEnd == 1) {
-                    cout << searchEnd << "\n";
-                    exit(0);
-                }
-                searchStart = searchPoint;
-            }
-            break;
-        case 0:
-            // 포인트 값이 원하는 값과 일치하는 경우 (자신은 참인데 그 상단이 N이 아닌 것은 가장 큰 N으로 나눠지는 값이라는 것)
-            if (getCount(K, searchPoint + 1) != N) {
-                cout << searchPoint << "\n";
-                exit(0);
-            }
-            // 포인트 값이 원하는 값과 일치하는데, 최대값이 아니라 반복이 필요 한 경우(searchPoint~최대값) 재탐색
-            else {
-                // 다음 값으로 이동해야하는데, 한칸만 이동해서 searchStart와 searchEnd의 차가 1이 되는 경우(다음 루프에서 반복적으로 한칸 내려옴)
-                if (searchEnd - searchPoint == 1 && getCount(K, searchPoint + 1) == N) {
-                    cout << searchEnd << "\n";
-                    exit(0);
-                }
-                if (searchStart - searchPoint == 1 && getCount(K, searchPoint + 1) == N) {
-                    cout << searchStart << "\n";
-                    exit(0);
-                }
-                searchStart = searchPoint;
-                searchEnd = maxLength;
-                algorithm(searchPoint, searchStart, searchEnd, K, N, maxLength);
-                continue;
-            }
-        }
-    }
-}
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int K, N;
-    cin >> K >> N;
-
-    int maxLength = 0;
-    for (int i = 0; i < K; i++) {
-        cin >> lanCable[i];
-        if (lanCable[i] > maxLength) maxLength = lanCable[i];
-    }
-
-    int searchEnd = maxLength;
-    int searchStart = 1;
-    int searchPoint = 0;
-
-    algorithm(searchPoint, searchStart, searchEnd, K, N, maxLength);
+	long long int maxAns = 0;
+	for (int i = 0; i < K; i++) {
+		cin >> arr[i];
+		maxAns = max(maxAns, arr[i]);			
+	}
+	sort(arr, arr + K);
+	long long int s = 1;
+	long long int e = maxAns;
+	while (s<=e) {
+		long long int m = (s + e) / 2;
+		long long int cnt = 0;
+		for (int i = 0; i < K; i++) {
+			cnt += arr[i] / m;
+		}
+		if (cnt >= N) {
+			s = m + 1;
+			maxAns = m;
+		}
+		else {
+			e = m - 1;
+		}
+	}
+	cout << maxAns << '\n';
 }
