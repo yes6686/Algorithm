bbc462b4a5be69e57e1cfab7dcbe30e5aee374e6
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-02-03 00:36:07 +0900

    [level 2] Title: 타겟 넘버, Time: 10.63 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,28 +1,24 @@
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-int arr[21];
-int ans =0;
-
-void dfs(int d,int n,int target,int size){
-    if(size==d){
-        if(n==target){
-            ans++;
-        }
-        return;
-    }
-    dfs(d+1,n+arr[d],target,size);
-    dfs(d+1,n-arr[d],target,size);
-}
-
 int solution(vector<int> numbers, int target) {
     int answer = 0;
-    for(int i=0;i<numbers.size();i++){
-        arr[i]= numbers[i];
+    
+    int k = (1<<numbers.size())-1;
+    for(int i=0;i<=k;i++){
+        int c = 0;
+        for(int j=0;j<numbers.size();j++){
+            if(i & (1<<j)) {
+                c += numbers[j];
+            }else{
+                c -= numbers[j];
+            }
+        }
+        if(c==target) answer++;
     }
-    dfs(0,0,target,numbers.size());
-    answer = ans;
+    
     return answer;
 }
