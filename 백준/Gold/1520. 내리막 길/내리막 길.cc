371a37aa052c8eb8b092fae03b118f8532d2c50d
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-02-06 13:12:38 +0900

    [Gold III] Title: 내리막 길, Time: 24 ms, Memory: 5516 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -1,39 +1,41 @@
 #include <iostream>
-#include <string.h>
 using namespace std;
 
-int arr[501][501];
-int dp[501][501];
-int n, m;
-
 int dx[4] = { 1,-1,0,0 };
-int dy[4] = { 0,0,1,-1 };
+int dy[4] = { 0, 0,1,-1 };
+int arr[501][501];
+long long int dp[501][501];
 
-int dfs(int x, int y) {
-	if (dp[x][y]!=-1) return dp[x][y];
-	if (x == n && y == m) return 1;
-	dp[x][y] = 0;
+int n, m;
+int dfs(int a, int b) {
+	if (a == 0 && b == 0) return 1;
+	if (dp[a][b]) return dp[a][b];
 
 	for (int i = 0; i < 4; i++) {
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-		if (nx<1 || ny<1 || nx >n || ny>m) continue;
-		if (arr[x][y] <= arr[nx][ny]) continue;
-		dp[x][y] += dfs(nx, ny);
+		int nx = a + dx[i];
+		int ny = b + dy[i];
+		if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+		if (arr[a][b] >= arr[nx][ny]) continue;
+		dp[a][b] += dfs(nx, ny);
 	}
-	return dp[x][y];
+	if (dp[a][b] == 0) {
+		arr[a][b] = 0;
+	}
+	return dp[a][b];
 }
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
+
 	cin >> n >> m;
-	
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m; j++) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
 			cin >> arr[i][j];
 		}
 	}
-	memset(dp, -1, sizeof(dp));
-	cout<<dfs(1, 1);
+
+	dfs(n-1, m-1);
+    
+	cout << dp[n - 1][m - 1] << '\n';
 }
