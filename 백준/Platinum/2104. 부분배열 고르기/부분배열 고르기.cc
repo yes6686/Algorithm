90dfba922d30cc8abd82869b6797ea9c21e92d0b
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-02-19 00:04:30 +0900

    [Platinum V] Title: 부분배열 고르기, Time: 1900 ms, Memory: 4524 KB -BaekjoonHub

---
+++ 백준/Platinum/2104. 부분배열 고르기/부분배열 고르기.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+long long int arr[100001];
+
+stack<pair<long long int, long long int>>st;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	st.push({ arr[0],0 });
+	long long int ans1 = 0;
+	for (int i = 1; i < n; i++) {
+		long long int vv, vi = i;
+		while (!st.empty()) {
+			long long int k = 0;
+			if (st.top().first > arr[i]) {
+				vi = st.top().second;
+				vv = st.top().first;
+				for (int j = st.top().second; j < i; j++) {
+					k += arr[j];
+				}
+				ans1 = max(ans1, k * vv);
+				st.pop();
+			}
+			else{
+				break;
+			}
+		}
+		st.push({ arr[i],vi });
+	}
+	while (!st.empty()) {
+		long long int k = 0;
+		for (int i = st.top().second; i < n; i++) {
+			k += arr[i];
+		}
+		ans1 = max(ans1, k * st.top().first);
+		st.pop();
+	}
+	cout << ans1 << '\n';
+	
+}
