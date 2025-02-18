f4c902f5a00f8517562aa8526ee18768d96b6924
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-02-19 00:09:07 +0900

    [Platinum V] Title: 퍼즐 자르기, Time: 16 ms, Memory: 4524 KB -BaekjoonHub

---
+++ 백준/Platinum/14727. 퍼즐 자르기/퍼즐 자르기.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+long long int arr[100001];
+
+stack<pair<long long int, long long int>>st;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	st.push({ arr[0],0 });
+	long long int ans = 0;
+	for (int i = 1; i < n; i++) {
+		long long int vv, vi = i;
+		while (!st.empty()) {
+			if (st.top().first > arr[i]) {
+				vi = st.top().second;
+				vv = st.top().first;
+				ans = max(ans, vv * (i - vi));
+				st.pop();
+			}
+			else {
+				break;
+			}
+		}
+		st.push({ arr[i],vi });
+	}
+	while (!st.empty()) {
+		ans = max(ans, (st.top().first * (n - st.top().second)));
+		st.pop();
+	}
+	cout << ans << '\n';
+
+}
