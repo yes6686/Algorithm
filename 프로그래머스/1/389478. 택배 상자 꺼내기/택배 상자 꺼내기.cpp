b622abb827ede16b629ad101a7a4d6ea1c50f174
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-02-19 22:59:52 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.03 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
@@ -1,49 +1,50 @@
+
 #include <string>
 #include <vector>
 
 using namespace std;
 
-int arr[102][102];
+int arr[101][101];
 
 int solution(int n, int w, int num) {
     int answer = 0;
-    int i = 1;
-    int j = 1;
+    int i = 1, j = 1;
     int s = 1;
-    int check = 0;
-    int ai,aj;
-    while(true){
-        if(check==0){
+    bool check = false; // 지그재그 여부
+    int ai = 0, aj = 0;
+
+    // 테이블을 채우는 과정
+    while (true) {
+        if (!check) {
             arr[i][j] = s;
-            if(s==num) {
+            if (s == num) {
                 ai = i;
                 aj = j;
             }
-        }else{
-            arr[i][w-j+1] = s;
-            if(s==num) {
+        } else {
+            arr[i][w - j + 1] = s;
+            if (s == num) {
                 ai = i;
-                aj = w-j+1;
+                aj = w - j + 1;
             }
         }
         s++;
-        if(s>n) break;
-        if(j==w){
+        if (s > n) break;
+        if (j == w) { // 다음 줄로 이동
             i++;
-            check = (check==1) ? 0 : 1;
-            j=1;
-        }else{
+            check = !check; // 방향 전환
+            j = 1;
+        } else {
             j++;
         }
     }
-    while(true){
-        if(arr[ai][aj]>=num){
-            answer++;
-            ai++;
-        }else{
-            break;
-        }
+
+    // 연속 증가하는 숫자 개수 세기
+    while (arr[ai][aj] >= num) {
+        answer++;
+        ai++;
+        if (ai > 100 || arr[ai][aj] == 0) break;
     }
-    
+
     return answer;
 }
