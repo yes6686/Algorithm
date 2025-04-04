a04a3daa038ffd3599a3232fc625b4bb64ece7ec
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-04-04 14:14:19 +0900

    [Bronze IV] Title: Quick Estimates, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/13297. Quick Estimates/Quick Estimates.cc
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+string a;
+int n;
+int main(){
+    cin >> n;
+    while (n--) {
+        cin >> a;
+        cout << a.size() << '\n';
+    }
+}
+ 
