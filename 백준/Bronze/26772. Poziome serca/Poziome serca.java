d095d461ff54791c67f2feb8f7427b32dd009518
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-04-13 22:07:13 +0900

    [Bronze IV] Title: Poziome serca, Time: 3464 ms, Memory: 73704 KB -BaekjoonHub

---
+++ 백준/Bronze/26772. Poziome serca/Poziome serca.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print(" @@@   @@@  ");
+		}
+		System.out.println();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print("@   @ @   @ ");
+		}
+		System.out.println();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print("@    @    @ ");
+		}
+		System.out.println();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print("@         @ ");
+		}
+		System.out.println();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print(" @       @  ");
+		}
+		System.out.println();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print("  @     @   ");
+		}
+		System.out.println();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print("   @   @    ");
+		}
+		System.out.println();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print("    @ @     ");
+		}
+		System.out.println();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print("     @      ");
+		}
+		System.out.println();
+		sc.close();
+	}
+}
