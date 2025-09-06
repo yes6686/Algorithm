27d194645601c2f2b3513ed490e16a07eaa7c09d
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-09-06 15:51:27 +0900

    [Bronze III] Title: 분수좋아해?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10474. 분수좋아해？/분수좋아해？.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	while (true) {
+		int a, b;
+		cin >> a >> b;
+		if (a == 0 && b == 0) break;
+
+		cout << a / b << " " << a - b*(a/b) << " / " << b << '\n';
+	}
+}
