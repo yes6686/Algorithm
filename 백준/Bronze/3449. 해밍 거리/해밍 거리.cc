54592f0fcc779983350a3b493f2d814b31180b0f
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-09-15 20:33:55 +0900

    [Bronze III] Title: 해밍 거리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/3449. 해밍 거리/해밍 거리.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	while (n--) {
+		string a, b;
+		cin >> a >> b;
+		int cnt = 0;
+		for (int i = 0;i < a.size();i++) {
+			if (a[i] != b[i]) cnt++;
+		}
+		cout << "Hamming distance is " << cnt <<"."<< '\n';
+	}
+}
