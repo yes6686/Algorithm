72a6de1e049a067665bcdfcedc448ec81a456c5a
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-11-22 12:51:28 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -4,19 +4,28 @@ using namespace std;
 int dp[1000001];
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
 	int n;
 	cin >> n;
-	
-	dp[1] = 0;
 
-	for (int i = 2; i <= n; i++) {
-		dp[i] = dp[i - 1] + 1;
-		if (i % 3 == 0) {
-			dp[i] = min(dp[i], dp[i / 3] + 1);
+	dp[2] = 1;
+	dp[3] = 1;
+
+	for (int i = 4;i <= n;i++) {
+
+		if (dp[i]) {
+			dp[i] = min(dp[i], dp[i - 1] + 1);
+			if (i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
+			if (i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1);
 		}
-		if (i % 2 == 0) {
-			dp[i] = min(dp[i], dp[i / 2] + 1);
+		else {
+			dp[i] = dp[i - 1] + 1;
+			if (i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
+			if (i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1);
 		}
 	}
-	cout << dp[n];
+	cout << dp[n] << '\n';
+
 }
