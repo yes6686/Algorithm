1b2ec9cf76daee38cd819de39ef30623bdab9586
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-11-28 02:17:42 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 2458.25 ms, Memory: 330 MB -BaekjoonHub

---
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+int visited[101][101][20001];
+
+pair<int,int>arr[101];
+pair<int,int>dis[101];
+
+int solution(vector<vector<int>> points, vector<vector<int>> routes) {
+    int answer=0;
+    
+    for(int i=0;i<points.size();i++){
+        dis[i+1].first = points[i][0];
+        dis[i+1].second = points[i][1];
+    }
+
+    for(int i=0;i<routes.size();i++){
+        arr[i+1].first = points[routes[i][0]-1][0];
+        arr[i+1].second = points[routes[i][0]-1][1];
+        visited[arr[i+1].first][arr[i+1].second][0]++;
+    }
+    
+        for(int i=1;i<=routes.size();i++){
+            int t=1;
+            for(int j=1;j<routes[i-1].size();j++){
+                int ei = dis[routes[i-1][j]].first;
+                int ej = dis[routes[i-1][j]].second;
+                while(true){
+                    int si = arr[i].first;
+                    int sj = arr[i].second;
+                    if(si==ei && sj==ej) break;
+                    if(si!=ei){
+                        if(si > ei){
+                            si--;
+                        }else if(si < ei){
+                            si++;
+                        }
+                        arr[i].first=si; 
+                        visited[si][sj][t]++;
+                    }else{
+                        if(sj!=ej){
+                            if(sj > ej){
+                                sj--;
+                            }else if(sj < ej){
+                                sj++;
+                            }
+                            arr[i].second=sj;
+                            visited[si][sj][t]++;
+                        }
+                    } 
+                    t++;
+                }
+            }
+        }
+        
+     for(int t=0;t<20001;t++){
+        for(int r=1;r<=100;r++){
+            for(int c=1;c<=100;c++){
+                if(visited[r][c][t] >= 2){
+                    answer++;
+                }
+            }
+        }
+    }
+    
+    
+    return answer;
+}
