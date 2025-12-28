fa5ad602ccf67603918a44ef6e2bbf1abb8a534b
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-12-29 00:20:27 +0900

    [level 2] Title: 올바른 괄호, Time: 18.56 ms, Memory: 54.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='('){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }else{
+                    stack.pop();
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
