cc0a7f78ef2c6066f2e1dec6c6179a46461cc7f0
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-12-29 13:24:45 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.37 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -2,35 +2,45 @@ import java.util.Stack;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        int answer = 0;
         
-        int r = board.length;
-        int c = board[0].length;
+        int n = board.length;
         
-        Stack<Integer>stack = new Stack<>();
-        
-        int n = moves.length;
-        int cnt = 0;
+        Stack<Integer>[] lanes = new Stack[n];
         
         for(int i=0;i<n;i++){
-            int k = moves[i] - 1;
-            for(int j=0;j<r;j++){
-                if(board[j][k]!=0){
-                    cnt++;
-                    if(stack.empty()) stack.push(board[j][k]);
-                    else{
-                        if(stack.peek() == board[j][k]){
-                            stack.pop();
-                        }else{
-                            stack.push(board[j][k]);
-                        }
-                    }
-                    board[j][k] =0;
-                    break;
+            lanes[i] = new Stack<>();
+        }
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<n;j++){
+                if(board[i][j] > 0){
+                    lanes[j].push(board[i][j]);
                 }
             }
         }
         
-        return cnt - stack.size();
+        Stack<Integer> bucket = new Stack<>();
+        
+        int answer=0;
+        
+        for(int move : moves){
+            int k = move - 1;
+            if(!lanes[k].isEmpty()){
+                int e = lanes[k].pop();
+                if(!bucket.isEmpty()){
+                    if(e==bucket.peek()){
+                        bucket.pop();
+                        answer+=2;
+                    }else{
+                        bucket.push(e);
+                    }
+                }else{
+                    bucket.push(e);
+                }
+            }
+        }
+        
+        
+        return answer;
     }
 }
