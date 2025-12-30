3b4dee8173a6af5f6070273235e67f257d8deb11
안예찬 <127647511+yes6686@users.noreply.github.com>
2025-12-30 14:13:14 +0900

    [level 2] Title: 예상 대진표, Time: 27.44 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 1;
+
+        int[] arr = new int[n];
+        
+        for(int i=0;i<n;i++) arr[i] = i+1;
+        
+        // 1 2 3 4 5 6 7 8
+        // 0 1 2 3 4 5 6 7
+        
+        while(true){
+            
+            int check=0;
+            for(int i=0;i<n;i+=2){
+                if((arr[i]==a && arr[i+1]==b) || (arr[i]==b && arr[i+1]==a)){
+                    check=1;
+                    break;
+                }
+                if(arr[i+1]==a){
+                    arr[(i+1)/2] = a;
+                }else if(arr[i+1]==b){
+                    arr[(i+1)/2] = b;
+                }else{
+                    arr[(i+1)/2] = arr[i];
+                }
+            }
+            if(check==1) break;
+            
+            n/=2;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
