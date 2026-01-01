1a7ec9070ff77ae7fbf03f04a25b3f0fc3330ff8
안예찬 <127647511+yes6686@users.noreply.github.com>
2026-01-01 16:33:43 +0900

    [Gold IV] Title: N-Queen, Time: 2604 ms, Memory: 17800 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class Main {
@@ -10,36 +9,38 @@ public class Main {
     private static boolean[] dra2;
 
     private static void bt(int row){
-
-        if(row == N){
+        if(row == N) {
             answer++;
             return;
         }
 
-        for(int col=0;col<N;col++){
-            if(!width[col] && !dra1[row + col] && !dra2[row - col + N]){
+        for(int col=0; col<N; col++){
+            if(!width[col] && !dra1[row+col] && !dra2[row-col+N]){
                 width[col] = true;
-                dra1[row + col] = true;
-                dra2[row - col + N] = true;
+                dra1[row+col] = true;
+                dra2[row-col+N] = true;
                 bt(row+1);
                 width[col] = false;
-                dra1[row + col] = false;
-                dra2[row - col + N] = false;
+                dra1[row+col] = false;
+                dra2[row-col+N] = false;
             }
         }
-
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         N = n;
+        
         width = new boolean[n];
         dra1 = new boolean[n*2];
         dra2 = new boolean[n*2];
+
         bt(0);
+
         System.out.println(answer);
+
         sc.close();
     }
 }
