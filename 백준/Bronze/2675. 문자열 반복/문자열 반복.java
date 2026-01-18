fd76ca5cbdece2eb3dfb6bd301c2bae48f21ff9e
안예찬 <127647511+yes6686@users.noreply.github.com>
2026-01-18 15:19:42 +0900

    [Bronze II] Title: 문자열 반복, Time: 188 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        for(int i=0;i<n;i++){
-            int r = scanner.nextInt();
-            String s = scanner.next();
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        for(int i=0;i<T;i++){
+            int r = sc.nextInt();
+            String s = sc.next();
             for(int j=0;j<s.length();j++){
+                char c = s.charAt(j);
                 for(int k=0;k<r;k++){
-                    System.out.print(s.charAt(j));
+                    System.out.print(c);
                 }
             }
             System.out.println();
